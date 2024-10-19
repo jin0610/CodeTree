b0b7b3bb4ecb67266faadf2140320fafcb6e5271
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-20 00:39:39 +0900

    Title: 배열 놀이, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241020/배열 놀이/play-with-array.py
+++ 241020/배열 놀이/play-with-array.py
@@ -1,16 +1,23 @@
 n, q = list(map(int, input().split()))
 nums = list(map(int, input().split()))
 
-quests = [list(map(int, input().split())) for i in range(q)]
+quests = []
+for i in range(q):
+    quests.append(list(map(int, input().split())))
 
 for quest in quests:
     if(quest[0] == 1):
-        print(nums[quest[1]-1])
-    elif(quest[0] == 2):
+        idx = quest[1] - 1
+        print(nums[idx])
+    elif(quest[0]==2):
         if(quest[1] in nums):
-            print(nums.index(quest[1]) + 1)
+            idx = nums.index(quest[1])
+            print(idx + 1)
         else:
             print(0)
     else:
-        for i in range(quest[1]-1, quest[2]):
+        n1 = quest[1]
+        n2 = quest[2]
+        for i in range(n1-1, n2):
             print(nums[i], end=" ")
+        print()
