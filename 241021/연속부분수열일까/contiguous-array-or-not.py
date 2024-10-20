25de2b7d364d895594b93b289afd79bf0c51629d
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-21 00:34:19 +0900

    Title: 연속부분수열일까, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 241021/연속부분수열일까/contiguous-array-or-not.py
+++ 241021/연속부분수열일까/contiguous-array-or-not.py
@@ -3,10 +3,9 @@ n1, n2 = list(map(int, input().split()))
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-seq_idx = []
 result = "No"
-for i in range(n1):
-    if result == "Yes" or (n1-i < n2-1):
+for i in range(n1-n2+1):
+    if result == "Yes":
         break
     
     if( result == "No" and A[i] != B[0]):
@@ -19,6 +18,4 @@ for i in range(n1):
             else:
                 break
     
-            
-
 print(result)
