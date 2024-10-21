dd210dbd832c43b4d53b8547865a3c31bd6cddb4
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-21 16:01:06 +0900

    Title: 격자 반대로 채우기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 241021/격자 반대로 채우기/grid-anti-fill.py
+++ 241021/격자 반대로 채우기/grid-anti-fill.py
@@ -2,21 +2,16 @@ n = int(input())
 
 result = [[0] * n for _ in range(n)]
 
-num = n*n
-for i in range(n):
-    for j in range(n):
-        if (n %2 == 1):
-            if(i % 2 == 0):
-                result[j][i] = num
-            else:
-                result[n-j-1][i] = num
-        else:
-            if(i % 2 == 0):
-                result[n-j-1][i] = num
-            else:
-                result[j][i] = num
+num = 1
 
-        num = num - 1
+for col in range(n-1, -1, -1):
+    for row in range(n-1, -1, -1):
+        if(n - 1 - col) % 2 == 0:
+            result[row][col] = num
+        else:
+            result[n-row-1][col] = num
+        num = num + 1
+        
 
 for i in range(n):
     print(*result[i])
