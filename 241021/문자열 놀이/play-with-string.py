2b7d352f066f44ff4782ed050e219fabf713948d
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-21 23:39:47 +0900

    Title: 문자열 놀이, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 241021/문자열 놀이/play-with-string.py
+++ 241021/문자열 놀이/play-with-string.py
@@ -1,10 +1,9 @@
-s, q = list(input().split())
-print(s,q)
+strs, q = list(input().split())
+
+strs = list(strs)
+
 for _ in range(int(q)):
-    print(_)
     i, a, b = list(input().split())
-    print(i, a, b)
-    strs = list(s)
 
     if(i == '1'):
         strs[int(a)-1], strs[int(b)-1] = strs[int(b)-1], strs[int(a)-1]
