bc153f54d5c6ebfa6b718a28a26fba376f9cb505
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-21 17:46:51 +0900

    Title: Run Length 인코딩, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 241021/Run Length 인코딩/run-length-encoding.py
+++ 241021/Run Length 인코딩/run-length-encoding.py
@@ -4,9 +4,6 @@ prev = A[0]
 cnt = 1
 result = ""
 
-if len(A) == 1:
-    result = prev + str(cnt)
-
 for i in range(1,len(A)):
     if(prev == A[i]):
         cnt = cnt + 1
@@ -16,8 +13,8 @@ for i in range(1,len(A)):
 
         prev = A[i]
         cnt = 1
-    if i == (len(A) - 1):
-        result = result + prev + str(cnt)
+
+result = result + prev + str(cnt)
 
 print(len(result))
 print(result)
