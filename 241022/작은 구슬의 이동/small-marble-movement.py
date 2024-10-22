740c641b1645c444c7a46d2477160bf9c7774e4c
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-22 17:03:40 +0900

    Title: 작은 구슬의 이동, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/작은 구슬의 이동/small-marble-movement.py
+++ 241022/작은 구슬의 이동/small-marble-movement.py
@@ -2,8 +2,8 @@ n, t = list(map(int, input().split()))
 r, c, d = input().split()
 
 
-# U D R L
-dx, dy = [0, 1, -1, 0], [1, 0, 0, -1]
+# U R L D
+dc, dr = [0, 1, -1, 0], [-1, 0, 0, 1]
 idx = {'U': 0, 'R' : 1, 'L' : 2, 'D' : 3}
 
 def in_range(x, y):
@@ -15,8 +15,10 @@ di = idx[d]
 r, c = int(r)-1, int(c)-1
 
 while sec != t:
-    if in_range(r + dy[di], c + dx[di]):
-        r, c = r + dy[di], c + dx[di]
+    nr, nc = r + dr[di], c + dc[di]
+
+    if in_range(nr, nc):
+        r, c = nr, nc
     else:
         di = 3 - di
     sec = sec + 1
