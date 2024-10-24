e58d4d914a717381ec325efdb119286281dfb9ac
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-25 00:07:04 +0900

    Title: 2개씩 그룹짓기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241025/2개씩 그룹짓기/group-of-pairs.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+nums = list(map(int, input().split()))
+
+nums.sort()
+
+result = 0
+for n in range(N):
+    num = nums[n] + nums[2*N - 1 - n]
+    if result > num:
+        result = num
+
+print(num)
