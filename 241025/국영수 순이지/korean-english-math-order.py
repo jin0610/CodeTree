327be37378b7ce41e9bf0c51aa3ae572912306ab
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-25 14:32:55 +0900

    Title: 국영수 순이지, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241025/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+n = int(input())
+
+scores = []
+for _ in range(n):
+    name, kor, eng, math = input().split()
+    kor, eng, math = list(map(int, [kor, eng, math]))
+    scores.append(Student(name, kor, eng, math)) 
+
+scores.sort(key=lambda x: (x.kor, x.eng, x.math), reverse=True)
+
+for i in range(n):
+    print(f"{scores[i].name} {scores[i].kor} {scores[i].eng} {scores[i].math}")
