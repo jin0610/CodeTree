78ffda85371805787511f9dd5ea81c35810ace50
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-26 00:04:38 +0900

    Title: Time to Time, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 241026/Time to Time/time-to-time.py
+++ 241026/Time to Time/time-to-time.py
@@ -1,18 +1,5 @@
 a, b, c, d = map(int, input().split())
-time = (c-a) * 60 + (d-b)
+
+time = (c -a ) * 60 + (d - b)
+
 print(time)
-# hours, mins = a, b
-# time = 0
-# while True:
-
-#     if a == c and b == d:
-#         print(time)
-#         break
-
-#     else:
-#         time += 1
-#         mins += 1
-
-#         if mins == 60:
-#             hours += 1
-#             mins = 0
