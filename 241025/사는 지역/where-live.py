4286f3e7e79478c42a23384a0f2cb214a0a924a8
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-25 11:33:13 +0900

    Title: 사는 지역, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 241025/사는 지역/where-live.py
+++ 241025/사는 지역/where-live.py
@@ -10,8 +10,8 @@ idx = 0
 info = []
 for i in range(n):
     name, address, region = input().split()
-
-    if name > info[idx].info:
+    info.append(Region(name, address, region))
+    if name > info[idx].name:
         idx = i
 
 print(f"name {info[idx].name}\naddr {info[idx].address}\ncity {info[idx].region}")
