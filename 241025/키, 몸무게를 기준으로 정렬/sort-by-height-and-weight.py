614a7d3f2df83f4b9ee05086ca09c8afeb090fbf
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-25 14:51:04 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241025/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(self, name, height, weight):
+        self.name= name
+        self.height = height
+        self.weight = weight
+
+n = int(input())
+students = []
+for i in range(5):
+    name, height, weight = input().split()
+    height, weight = int(height), int(weight)
+    students.append(Student(name, height, weight)) 
+
+students.sort(key=lambda x: (x.height, -x.weight))
+
+for i in range(n):
+    print(f"{students[i].name} {students[i].height} {students[i].weight}")
