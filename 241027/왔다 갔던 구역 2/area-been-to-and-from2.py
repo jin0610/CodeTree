90907a05d7c33423ae03bfb33046e4b7bfaed573
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-27 01:02:35 +0900

    Title: 왔다 갔던 구역 2, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 241027/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241027/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -7,18 +7,19 @@ for _ in range(n):
     dist, direct = input().split()
     dist = int(dist)
 
-    # print(_, start, direct, dist)
+    #print(_, start, direct, dist)
     if direct == "R":
-        for i in range(1, dist + 1):
+        for i in range(0, dist):
             location[start + i] += 1
         start += dist
     else:
         for i in range(1, dist + 1):
             location[start - i] += 1
         start -= dist
-
+# print(location)
 cnt = 0
 for l in location:
     if l >= 2:
         cnt += 1
+        
 print(cnt)
