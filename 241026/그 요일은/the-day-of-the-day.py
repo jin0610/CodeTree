471e13d04b42469e97129c773c85ff81ed77105b
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-26 18:31:45 +0900

    Title: 그 요일은, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241026/그 요일은/the-day-of-the-day.py
+++ 241026/그 요일은/the-day-of-the-day.py
@@ -5,11 +5,8 @@ m_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 day = (sum(m_days[:m2]) + d2) - (sum(m_days[:m1]) + d1)
-# print(day, day //7, day %7, days.index(A))
+
 if (day % 7) >= days.index(A):
-    if(day // 7) == 0:
-        print(day // 7)
-    else:
         print(day // 7 + 1)
     
 else:
