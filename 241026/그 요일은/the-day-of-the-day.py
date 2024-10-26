38bb7604e90019f40647ea0a765b5fa25f921a4e
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-26 18:49:00 +0900

    Title: 그 요일은, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 241026/그 요일은/the-day-of-the-day.py
+++ 241026/그 요일은/the-day-of-the-day.py
@@ -1,7 +1,7 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
-m_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+m_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 day = (sum(m_days[:m2]) + d2) - (sum(m_days[:m1]) + d1)
