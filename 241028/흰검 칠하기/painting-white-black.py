fdefe6e49d18fe7dfd27c758c46eab37fb61c8b8
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-28 01:53:50 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241028/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,48 @@
+n = int(input())
+
+offset = 1000
+color = [[0,0,0] for _ in range(100000)]
+start = 100000 // 2
+
+for _ in range(n):
+    dist, direct = input().split()
+    dist = int(dist)
+    
+    if direct == "R":
+        for j in range(0, dist):
+            
+            if color[start + j][0] == "G":
+                pass
+            else:
+                color[start + j][1] += 1
+                if color[start + j][1] >= 2 and color[start + j][2] >= 2:
+                    color[start + j][0] = "G"
+                else:
+                    color[start + j][0] = "B"
+        start += (dist - 1)
+                
+    else:
+        for j in range(0, dist):
+            
+            if color[start - j][0] == "G":
+                pass
+            else:
+                color[start - j][2] += 1
+                if color[start - j][1] >= 2 and color[start - j][2] >= 2:
+                    color[start - j][0] = "G"
+                else:
+                    color[start - j][0] = "W"
+        start -= (dist - 1)
+
+w, b, g = 0,0,0
+for c in color:
+    if c[0] == 'W':
+        w += 1
+    elif c[0] == "B":
+        b += 1
+    elif c[0] == "G":
+        g += 1
+    else:
+        pass
+
+print(w, b, g)
