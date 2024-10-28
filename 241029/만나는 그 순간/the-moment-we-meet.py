cc97e0b804705328f43217434ab844c30179b700
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-29 00:52:22 +0900

    Title: 만나는 그 순간, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241029/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,33 @@
+import sys
+
+N, M = map(int, input().split())
+
+A, B = [0],[0]
+a, b = 0, 0
+
+for _ in range(N):
+    d, t = input().split()
+    t = int(t)
+
+    for i in range(t):
+        if d == "R":
+            a += 1
+        else:
+            a -= 1
+        A.append(a)
+
+for _ in range(M):
+    d, t = input().split()
+    t = int(t)
+
+    for i in range(t):
+        if d == "R":
+            b += 1
+        else:
+            b -= 1
+        B.append(b)
+
+for i in range(1, len(A)):
+    if A[i] == B[i]:
+        print(i)
+        break
