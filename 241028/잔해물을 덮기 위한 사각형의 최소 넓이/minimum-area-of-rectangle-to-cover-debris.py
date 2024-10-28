aeebcb7c814ef5fdaf19ec62c4d2aece49346edf
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-28 17:32:32 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 164ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 241028/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 241028/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -5,15 +5,20 @@ arr = [[0] * 2000 for _ in range(2000)]
 x1, y1, x2, y2 = map(lambda x : int(x) + 1000, input().split())
 a1, b1, a2, b2 = map(lambda x : int(x) + 1000, input().split())
 
+# 두 사각형이 겹친 경우
+if x1 >= a1 and x2 <= a2 and y1 >= b1 and y2 <= b2:
+    print(0)
+    sys.exit(0)
+
 # 첫번째 사각형 
 for x in range(x1, x2):
     for y in range(y1, y2):
-        arr[x][y] = 1
+        arr[x][y] += 1
 
 # 두번째 사각형
 for x in range(a1, a2):
     for y in range(b1, b2):
-        arr[x][y] = 2
+        arr[x][y] += 2
 
 min_x, min_y, max_x, max_y = 2000, 2000, -1000, -1000
 for x in range(2000):
@@ -24,4 +29,5 @@ for x in range(2000):
             max_x = max(max_x, x)
             max_y = max(max_y, y)
 
+print(min_x, min_y, max_x, max_y)
 print((max_x - min_x + 1) * (max_y-min_y + 1))
