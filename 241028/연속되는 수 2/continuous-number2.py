fd61b27462b6abb57e3a66d9bb4e43aef3e2c17b
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-28 17:54:49 +0900

    Title: 연속되는 수 2, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 241028/연속되는 수 2/continuous-number2.py
+++ 241028/연속되는 수 2/continuous-number2.py
@@ -1,9 +1,16 @@
+import sys
 N =int(input())
 
 nums = [ int(input()) for _ in range(N)]
+
 max_cnt = 0
 cnt = 1
+if N == 1:
+    print(1)
+    sys.exit(0)
+    
 for n in range(1, N):
+    
     if nums[n] == nums[n-1]:
         cnt += 1
     else:
