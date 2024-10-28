999b5dbf613b99575d10b36e9b78488f5d1f18a6
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-28 17:29:50 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 185ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 241028/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 241028/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -5,38 +5,23 @@ arr = [[0] * 2000 for _ in range(2000)]
 x1, y1, x2, y2 = map(lambda x : int(x) + 1000, input().split())
 a1, b1, a2, b2 = map(lambda x : int(x) + 1000, input().split())
 
-x_length, y_length, a_length, b_length = x2-x1, y2-y1, a2-a1, b2-b1
-
-
-# 두 사각형이 완전히 겹칠 경우
-if x1 >= a1 and x2 <= a2 and y1 >= b1 and y2 <= b2:
-    print(0)
-    sys.exit(0)
-# 두 사각형이 겹치지 않을 경우 첫번 째 직사각형의 넓이 출력
-elif (x_length + a_length) >= (a2 - x1) or (y_length + b_length) >= (b2-y1):
-    print((x2-x1) * (y2-y1))
-    sys.exit(0)
-
-
-
-
+# 첫번째 사각형 
 for x in range(x1, x2):
     for y in range(y1, y2):
         arr[x][y] = 1
 
+# 두번째 사각형
 for x in range(a1, a2):
     for y in range(b1, b2):
         arr[x][y] = 2
 
-min_x, min_y, max_x, max_y = x1, y1, x2, y2
-for x in range(x1, x2):
-    for y in range(y1, y2):
-        
+min_x, min_y, max_x, max_y = 2000, 2000, -1000, -1000
+for x in range(2000):
+    for y in range(2000):
         if arr[x][y] == 1:
             min_x = min(min_x, x)
             min_y = min(min_y, y)
             max_x = max(max_x, x)
             max_y = max(max_y, y)
-  
-print(min_x, min_y, max_x, max_y)
-print((max_x - min_x) * (max_y-min_y))
+
+print((max_x - min_x + 1) * (max_y-min_y + 1))
