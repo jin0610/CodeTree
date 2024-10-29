2fd578552a53dbfa922ded032483600e12097c41
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-29 13:57:23 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241029/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 241029/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -1,6 +1,6 @@
 n, m  = map(int, input().split())
 
-arr = [[0 for i in range(n)] for _ in range(m)]
+arr = [[0 for i in range(m)] for _ in range(n)]
 
 # 우 하 좌 상
 dr, dc = [0, 1, 0, -1], [ 1, 0, -1, 0]
@@ -9,16 +9,20 @@ arr[r][c] = 1
 idx = 0
 
 def in_range(x, y):
-    return x >= 0 and x < m and y >= 0 and y < n
+    return x >= 0 and x < n and y >= 0 and y < m
 
 for i in range(1, n * m):
     nr, nc = r + dr[idx], c + dc[idx]
-    if in_range(nr, nc) and arr[nr][nc] == 0:
-        r, c = nr, nc
+
+    if in_range(nr, nc) :
+        if arr[nr][nc] == 0:
+            r, c = nr, nc
+            
     else:
         idx = (idx + 1) % 4
         r, c = r + dr[idx], c +dc[idx]
     arr[r][c] = i + 1
+
 
 for i in range(n):
     print(*arr[i])
