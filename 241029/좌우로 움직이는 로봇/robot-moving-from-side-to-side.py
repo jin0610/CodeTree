dc40ef0a265186af322887209106142fcac85f94
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-29 11:01:19 +0900

    Title: 좌우로 움직이는 로봇, Time: 286ms, Memory: 63MB, Status: Passed - CodeTree

--- 241029/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 241029/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,12 +1,12 @@
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 
-A = [0]
-B = [0]
+A, B = [0], [0]
 a, b = 0, 0
+
 for _ in range(n):
     t, d = input().split()
     t = int(t)
-    
+
     for i in range(t):
         if d == "R":
             a += 1
@@ -17,7 +17,7 @@ for _ in range(n):
 for _ in range(m):
     t, d = input().split()
     t = int(t)
-    
+
     for i in range(t):
         if d == "R":
             b += 1
@@ -25,12 +25,19 @@ for _ in range(m):
             b -= 1
         B.append(b)
 
+cnt = 0
 
-cnt = 1
-for i in range(1, min(len(A),len(B))):
-    if B[i] == A[i]:
-        if B[i-1] != A[i-1]:
-            cnt += 1
-    else:
-        pass
+for i in range(1, max(len(A), len(B))):
+    try:
+        if A[i] == B[i]:
+            if A[i-1] != B[i-1]:
+                cnt +=1
+    except:
+        if len(A) > len(B):
+            if A[i] == B[-1]:
+                
+                cnt +=1
+        else:
+            if A[-1] == B[i]:
+                cnt +=1
 print(cnt)
