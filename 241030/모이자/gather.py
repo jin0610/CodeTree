a44e791dbe0fd6766a1ea688b0f765fed3aee1a8
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-10-30 15:20:51 +0900

    Title: 모이자, Time: 158ms, Memory: 25MB, Status: Passed - CodeTree

--- 241030/모이자/gather.py
+++ 241030/모이자/gather.py
@@ -3,12 +3,11 @@ import sys
 n = int(input())
 arr = list(map(int, input().split()))
 
-min_diffs = []
+
 min_diff = sys.maxsize
 for i in range(n):
     dist = 0
     for j in range(n):
-
         dist = dist + arr[j] * abs(i-j)
 
     min_diff = min(dist, min_diff)
