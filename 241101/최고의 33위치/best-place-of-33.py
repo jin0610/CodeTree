c0608fb37770067b1853fdff671d202b9b4f23d0
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-11-01 21:23:10 +0900

    Title: 최고의 33위치, Time: 48ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241101/최고의 33위치/best-place-of-33.py
+++ 241101/최고의 33위치/best-place-of-33.py
@@ -4,7 +4,7 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 
 def solution(arr):
     N = len(arr)
-    arr2 = [[0]*(N-2) for _ in range(N)]
+    arr2 = [[0]*(N-2) for _ in range(N-2)]
     
     if(N == 3):
         arr2[0][0] = sum(arr[0]) + sum(arr[1]) + sum(arr[2])
@@ -13,9 +13,10 @@ def solution(arr):
     max_cnt = 0
     for i in range(N-2):
         for j in range(N-2):
-            arr2[i][j] = sum(arr[i]) + sum(arr[i+1]) +sum(arr[i+2])
+            print(i,j)
+            arr2[i][j] = sum(arr[i][i:i+3]) + sum(arr[i+1][i:i+3]) +sum(arr[i+2][i:i+3])
             max_cnt = max(arr2[i][j], max_cnt)
-
+    
     return max_cnt
 
 print(solution(arr))
