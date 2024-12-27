6d4599c7d2760ed41626223f5fadedc2d8de7e55
jin0610 <87145895+jin0610@users.noreply.github.com>
2024-12-27 16:59:51 +0900

    Title: 체크판위에서 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241227/체크판위에서 2/on-the-checkboard-2.py
+++ 241227/체크판위에서 2/on-the-checkboard-2.py
@@ -1,13 +1,14 @@
 R, C = map(int, input().split())
+grid = [input().split() for _ in range(R)]
 
-grid = [ list(input().split()) for _ in range(R)]
+count = 0
 
-result = 0
 for i in range(1, R):
     for j in range(1, C):
-        for r in range(i + 1, R-1):
-            for c in range(j + 1, C-1):
-                if grid[0][0] != grid[i][j] and grid[i][j] != grid[r][c] and grid[r][c] != grid[R-1][C-1]:
-                    result += 1
+        for r in range(i + 1, R - 1):
+            for c in range(j + 1, C - 1):
+                if grid[i][j] != grid[0][0] and grid[r][c] != grid[R-1][C-1]:
+                    if grid[r][c] != grid[i][j]:
+                        count += 1
 
-print(result)
+print(count)
