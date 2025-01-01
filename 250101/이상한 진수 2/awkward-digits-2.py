35dbc61361f8878b6082c5c4610f52a2d528c323
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-01 23:43:08 +0900

    Title: 이상한 진수 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250101/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,18 @@
+### 자리 수 단위로 완전탐색 / 이상한 진수2
+
+a = input()
+
+N = int(a,2)
+
+for i in range(len(a)):
+    
+    if a[i] == '1':
+        num = a[:i] + '0' + a[i+1:]
+        
+    else:
+        num = a[:i] + '1' + a[i+1:]
+
+    N = max(N, int(num,2))  
+
+print(N)
+        
