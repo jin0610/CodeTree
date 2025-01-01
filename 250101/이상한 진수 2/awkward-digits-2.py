c6ccdb08c4c340170bb96f4ca6f27790bab80932
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-01 23:50:44 +0900

    Title: 이상한 진수 2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250101/이상한 진수 2/awkward-digits-2.py
+++ 250101/이상한 진수 2/awkward-digits-2.py
@@ -1,48 +1,21 @@
 ### 자리 수 단위로 완전탐색 / 이상한 진수2
 
-import sys
+a = input()
 
-INT_MIN = -sys.maxsize
+result = -1
 
-# 변수 선언 및 입력
-binary = list(map(int, list(input())))
-length = len(binary)
+for i in range(len(a)):
 
-# 각 i번째 자릿수를 바꾸었을 때의 십진수 값을 구해줍니다.
-ans = INT_MIN
-for i in range(length):
-	# i번째 자릿수를 바꿉니다.
-	binary[i] = 1 - binary[i]
-	
-	# 십진수로 변환합니다.
-	num = 0
-	for j in range(length):
-		num = num * 2 + binary[j]
-	
-	ans = max(ans, num)
-	
-	# i번째 자릿수를 원래대로 돌려놓습니다.
-	binary[i] = 1 - binary[i]
+    ## if문을 사용하지 않은 풀이 44ms
+    num = a[:i] + str(1 - int(a[i])) + a[i+1:]
 
-# 출력
-print(ans)
+    ## if문을 사용하는 풀이 43ms
+    # if a[i] == '1':
+    #     num = a[:i] + '0' + a[i+1:]
+    # else:
+    #     num = a[:i] + '1' + a[i+1:]
 
-# a = input()
+    result = max(result, int(num,2))  
 
-# result = -1
-
-# for i in range(len(a)):
-
-#     ## if문을 사용하지 않은 풀이 44ms
-#     num = a[:i] + str(1 - int(a[i])) + a[i+1:]
-
-#     ## if문을 사용하는 풀이 43ms
-#     # if a[i] == '1':
-#     #     num = a[:i] + '0' + a[i+1:]
-#     # else:
-#     #     num = a[:i] + '1' + a[i+1:]
-
-#     result = max(result, int(num,2))  
-
-# print(result)
+print(result)
         
