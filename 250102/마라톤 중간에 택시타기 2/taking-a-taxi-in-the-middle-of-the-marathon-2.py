08ce306179d32ec79b7e1c484ff32392884835d6
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-02 15:47:19 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250102/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -7,14 +7,17 @@ points = [ input().split() for _ in range(N)]
 result = sys.maxsize
 
 for i in range(1, N-1):
-    point = points.copy()
-    length = 0
-    point.pop(i)
-
-    for j in range(1,len(point)):
-        x1, y1 = map(int,point[j-1])
-        x2, y2 = map(int,point[j])
-        length = length + (abs(x1 - x2) + abs(y1- y2))
+    distance = 0
+    prev_idx = 0
     
-    result = min(result, length)
+    for j in range(1,N):
+        if j == i:
+            continue
+        
+        x1, y1 = map(int,points[prev_idx])
+        x2, y2 = map(int,points[j])
+        distance += (abs(x1 - x2) + abs(y1- y2))
+        prev_idx = j
+    
+    result = min(result, distance)
 print(result)
