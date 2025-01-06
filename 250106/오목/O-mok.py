b4c563bded70701d0e8849e15c0833040c9f7db7
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-06 23:46:17 +0900

    Title: 오목, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250106/오목/O-mok.py
+++ 250106/오목/O-mok.py
@@ -25,7 +25,7 @@ for row in range(14):
                 sys.exit()
 
             # 대각선 비교
-            colors = [board[row+i][col-i] for i in range(5)]
+            colors = [board[row+i][col+i] for i in range(5)]
             if len(set(colors)) == 1:
                 print(color)
                 print(row + 3, col + 3)
