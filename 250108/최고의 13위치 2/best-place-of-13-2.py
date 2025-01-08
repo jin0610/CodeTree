978e738157c5aa2d7f549ffe8f2833748ab0382d
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-08 16:07:22 +0900

    Title: 최고의 13위치 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250108/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,25 @@
+### 자리 수 단위로 완전 탐색 / 최고의 13 위치 2
+import sys
+
+N = int(input())
+arr = [ list(map(int, input().split())) for _ in range(N)] 
+
+def in_range(x, y):
+    return x >= 0 and y >= 0 and x < N and y < N
+
+result = -sys.maxsize
+for i in range(N):
+    for j in range(N-2):
+        for k in range(N):
+            for l in range(N-2):
+                if k == i and l >= j and l <= j + 2:
+                    continue
+
+                cnt1 = sum(arr[i][j:j + 3])
+                cnt2 = sum(arr[k][l : l + 3])
+                result = max(result, cnt1 +  cnt2)
+        
+        
+        
+
+print(result)
