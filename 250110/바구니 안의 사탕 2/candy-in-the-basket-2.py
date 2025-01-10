dc11dae4689e9d39b975726a07682958c7f53dd8
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-10 15:56:45 +0900

    Title: 바구니 안의 사탕 2, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250110/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,19 @@
+### 구간 단위로 완전탐색 / 바구니 안의 사탕2
+N,K = map(int, input().split())
+candy_cnt = []
+candy_index = []
+for _ in range(N):
+    c, i = map(int, input().split())    # [각 사탕의 개수, 바구니의 좌표]
+    candy_cnt.append(c)
+    candy_index.append(i)
+
+candy = [0] * (max(candy_index)+1)
+
+result = 0
+for c, i in zip(candy_cnt, candy_index):
+    candy[i] += c
+
+for i in range(K, len(candy)-K+1):
+    result = max(result, sum(candy[i-K : i+K+1]))
+
+print(result)
