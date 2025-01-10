ef96c5e82227e920a8b81d6ad3d4c18aeb7226f1
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-11 02:09:36 +0900

    Title: 개발자의 능력 3, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250111/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,15 @@
+### 자리마다 숫자를 정하는 완전탐색 / 개발자의 능력 3
+import sys
+abilities = list(map(int, input().split()))
+
+# Write your code here!
+min_diff = sys.maxsize
+sum_abilities = sum(abilities)
+for i in range(6):
+    for j in range(i, 6):
+        for k in range(j, 6):
+            num = abilities[i] + abilities[j] + abilities[k]
+            num2 = sum_abilities - num
+            min_diff = min(min_diff, abs(num-num2))
+
+print(min_diff)
