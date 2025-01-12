5631e3ba8687376d54c1239244ba04ec367593a7
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-12 22:06:46 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250112/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 250112/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -1,23 +1,46 @@
 ### 자리마다 숫자를 정하는 완전탐색 / 두 가지로 열리는 자물쇠
+import sys
 N = int(input())
 a1, b1, c1 = map(int, input().split())
 a2, b2, c2 = map(int, input().split())
 
 # Write your code here!
-# 겹치는 경우
-cnt_a, cnt_b, cnt_c = 0, 0, 0
-for n in range(N+2):
-    if abs(a1-n) <= 2 and abs(a2 - n) <= 2:
-        cnt_a += 1
-    if abs(b1-n) <= 2 and abs(b2 - n) <= 2:
-        cnt_b += 1
-    if abs(c1-n) <= 2 and abs(c2 - n) <= 2:
-        cnt_c += 1
+cnt1 = [0,0,0]  # 첫 번쩨 조합의 수
+cnt2 = [0,0,0]  # 두 번쩨 조합의 수
+cnt3 = [0,0,0]  # 중복된 조합의 수
 
-cnt = cnt_a * cnt_b * cnt_c
-print(250-cnt)
+def is_in_range(N, i, x):
+    if abs(i-x) <= 2 or abs(i-x) >= N-2:
+        return True
+    return False
 
-        
-    
-    
+for i in range(1, N + 1):
+    if is_in_range(N, i, a1):
+        cnt1[0] += 1
+    if is_in_range(N, i, b1):
+        cnt1[1] += 1
+    if is_in_range(N, i, c1):
+        cnt1[2] += 1
+
+    if is_in_range(N, i, a2):
+        cnt2[0] += 1
+    if is_in_range(N, i, b2):
+        cnt2[1] += 1
+    if is_in_range(N, i, c2):
+        cnt2[2] += 1
+
+    if is_in_range(N, i, a1) and is_in_range(N, i, a2):
+        cnt3[0] += 1
+    if is_in_range(N, i, b1) and is_in_range(N, i, b2):
+        cnt3[1] += 1
+    if is_in_range(N, i, c1) and is_in_range(N, i, c2):
+        cnt3[2] += 1
+
+comb1, comb2, comb3 = 1,1,1
+for i in range(3):
+    comb1 *= cnt1[i]
+    comb2 *= cnt2[i]
+    comb3 *= cnt3[i]
+
+print(comb1 + comb2 - comb3)
 
