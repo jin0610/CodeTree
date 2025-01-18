c6d49c3fc09a37ee7be0235796f22b1c0afc0d78
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-18 22:37:47 +0900

    Title: 삼각형 만들기, Time: 91ms, Memory: 18MB, Status: Passed - CodeTree

--- 250118/삼각형 만들기/create-triangle.py
+++ 250118/삼각형 만들기/create-triangle.py
@@ -5,15 +5,15 @@ points = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Write your code here!
 max_area = 0
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            if i != j and j != k and k != i:
-                x1, y1 = points[i]
-                x2, y2 = points[j]
-                x3, y3 = points[k]
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(j +1 ,n):
+            # if i != j and j != k and k != i:
+            x1, y1 = points[i]
+            x2, y2 = points[j]
+            x3, y3 = points[k]
 
-                if (x1 == x2 and y2 == y3) or (x1 == x3 and y1 == y2):
-                    S = abs((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3))
-                    max_area = max(S, max_area)
+            if (x1 == x2 or x1 == x3 or x2 == x3) and (y1 == y2 or y1 == y3 or y2 == y3):
+                S = abs((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3))
+                max_area = max(S, max_area)
 print(max_area)
