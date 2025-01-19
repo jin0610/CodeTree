4d1644903bcc9a9eaf0be3b7fa03f90cd39f658c
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-19 23:17:53 +0900

    Title: 운행 되고 있는 시간, Time: 63ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250119/운행 되고 있는 시간/hours-in-service.py
@@ -0,0 +1,21 @@
+### 물체 단위로 완전탐색 / 운행 되고 있는 시간
+n = int(input())
+times = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Write your code here!
+
+max_times = 0
+for i in range(n):
+    time_list = [0] * 1001
+    for j in range(n):
+        if i == j:
+            continue
+        for t in range(times[j][0],times[j][1]):
+            if time_list[t] == 0:
+                time_list[t] = 1
+                
+    time = sum(time_list)
+    max_times = max(max_times, time)
+              
+
+print(max_times)
