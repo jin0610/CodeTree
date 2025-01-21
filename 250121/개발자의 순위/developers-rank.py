5981bdde220291b6faccc0db0f077f5138c6ad75
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-21 22:15:48 +0900

    Title: 개발자의 순위, Time: 56ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250121/개발자의 순위/developers-rank.py
@@ -0,0 +1,21 @@
+K, N = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(K)]
+
+# Write your code here!
+cnt = 0
+for i in range(N):
+    
+    for j in range(N):
+        flag = True
+        if i == j:
+            continue
+        for a in arr:
+            idx_i, idx_j = a.index(i+1), a.index(j+1)
+            if idx_i > idx_j:
+                flag = False                
+
+        if flag == True:
+            cnt += 1
+
+print(cnt)
+
