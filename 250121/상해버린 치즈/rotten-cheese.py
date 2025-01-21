7a0353f511c160f4c92ce201a7eadaaa22dba325
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-21 16:02:06 +0900

    Title: 상해버린 치즈, Time: 78ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250121/상해버린 치즈/rotten-cheese.py
+++ 250121/상해버린 치즈/rotten-cheese.py
@@ -3,19 +3,42 @@ N, M, D, S = map(int, input().split())
 info = [tuple(map(int, input().split())) for _ in range(D)] # (p, m, t) : (몇 번째 사람(p), 몇 번째 치즈(m), 언제먹었는지(t초))
 sick = [tuple(map(int, input().split())) for _ in range(S)] # (p, t) : (몇 번째 사람(p),언제 아팠는지(t초))
 info = sorted(info, key = lambda x : x[-1])
-sick = sorted(sick, key = lambda x : x[-1])
 
 sick_p = []
-cheese = []
+strange = []
 
 for i in range(S):
     sp, st = sick[i]
     for j in range(D):
         dp, dm, dt = info[j]
-        if sp == dp:
-            cheese.append(dm)
-        if dm in cheese or st > dt:
-            sick_p.append(dp)
+        if sp == dp and st > dt:
+            strange.append([dm, dt]) 
 
+for p,m,t in info:
+    for sc, st in strange:
+        if m == sc and t >= st:
+            sick_p.append(p)
+            
 medicine_cnt = len(set(sick_p))
 print(medicine_cnt)
+
+# N, M, D, S = map(int, input().split())
+# info = [tuple(map(int, input().split())) for _ in range(D)] # (p, m, t) : (몇 번째 사람(p), 몇 번째 치즈(m), 언제먹었는지(t초))
+# sick = [tuple(map(int, input().split())) for _ in range(S)] # (p, t) : (몇 번째 사람(p),언제 아팠는지(t초))
+# info = sorted(info, key = lambda x : x[-1])
+# sick = sorted(sick, key = lambda x : x[-1])
+
+# sick_p = []
+# cheese = []
+
+# for i in range(S):
+#     sp, st = sick[i]
+#     for j in range(D):
+#         dp, dm, dt = info[j]
+#         if sp == dp:
+#             cheese.append(dm)
+#         if dm in cheese or st > dt:
+#             sick_p.append(dp)
+
+# medicine_cnt = len(set(sick_p))
+# print(medicine_cnt)
