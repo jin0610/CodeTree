d245f133c99030b801bb7712d6c01dcf28311c6e
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-23 23:46:52 +0900

    Title: 스승의 은혜 3, Time: 145ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 250123/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250123/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,23 +1,25 @@
 ### 물체 단위로 완전탐색 / 스승의 은혜 3
 N, B = map(int, input().split())
 gifts = [list(map(int, input().split())) for _ in range(N)]
-gifts = sorted(gifts, key= lambda x : (x[0]//2 + x[1]))
 
 # Write your code here!
 max_cnt = 0
 for i in range(N):
-    money = B
-    p, s = gifts[i]
-    p = p // 2
-    money -= (p + s)
-    cnt = 1
+    temp = gifts.copy()
+
+    temp[i][0] /= 2
+
+    prices = [(temp[k][0] +temp[k][1] ) for k in range(N)]
+    prices.sort()
+    
+    student = 0 
+    price = 0
+
     for j in range(N):
-        if i == j:
-            continue        
-        if money - sum(gifts[j]) < 0:  
+        if price + prices[j] > B:
             break
-        cnt += 1
-        money -= sum(gifts[j])
-    max_cnt = max(max_cnt, cnt)
+        price += prices[j]
+        student += 1
+    max_cnt = max(max_cnt, student)
 
 print(max_cnt)
