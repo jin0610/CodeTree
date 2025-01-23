234dc46dbd7b3069e28b0c75c964bc95521b6550
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-23 23:34:11 +0900

    Title: 스승의 은혜 3, Time: 66ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250123/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250123/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,7 +1,7 @@
 ### 물체 단위로 완전탐색 / 스승의 은혜 3
 N, B = map(int, input().split())
 gifts = [tuple(map(int, input().split())) for _ in range(N)]
-gifts = sorted(gifts, key= lambda x : (x[0]//2 + x[1], x[0], x[1]))
+gifts = sorted(gifts, key= lambda x : (x[0]//2 + x[1]))
 
 # Write your code here!
 max_cnt = 0
