685b272ad7b62db9393b9a388d5066ac2f796ad0
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-23 23:39:37 +0900

    Title: 스승의 은혜 3, Time: 84ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250123/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250123/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -6,18 +6,20 @@ gifts = sorted(gifts, key= lambda x : (x[0]//2 + x[1]))
 # Write your code here!
 max_cnt = 0
 for i in range(N):
-    cnt = 0
     money = B
+    p, s = gifts[i]
+    p = p // 2
+    money -= (p + s)
+    cnt = 1
     for j in range(N):
-        p, s = gifts[j]
         if i == j:
-            p = p // 2
-        money -= (p + s)
-        cnt += 1
-        if money < 0:  
-            cnt -= 1
-            break
+            continue
+        money -= sum(gifts[j])
         
+        if money < 0:  
+        
+            break
+        cnt += 1
     max_cnt = max(max_cnt, cnt)
 
 print(max_cnt)
