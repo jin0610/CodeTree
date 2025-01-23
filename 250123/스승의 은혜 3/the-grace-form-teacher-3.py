15ed67dbe20c78e0e57c404e4fecdc3c99ce5b1f
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-01-23 23:05:48 +0900

    Title: 스승의 은혜 3, Time: 84ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250123/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250123/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,8 +1,7 @@
 ### 물체 단위로 완전탐색 / 스승의 은혜 3
 N, B = map(int, input().split())
 gifts = [tuple(map(int, input().split())) for _ in range(N)]
-P = [gift[0] for gift in gifts]
-S = [gift[1] for gift in gifts]
+gifts = sorted(gifts, key= lambda x : (x[0]//2 + x[1], x[0], x[1]))
 
 # Write your code here!
 max_cnt = 0
@@ -10,13 +9,19 @@ for i in range(N):
     cnt = 0
     money = B
     for j in range(N):
-        p, s = gifts[j]
+        # print("before:",i, j, p, s)
         if i == j:
+            p, s = gifts[i]
             p = p // 2
-        money -= (p + s)
-        if money < 0:
+            money -= (p + s)
+        else:
+            money -= sum(gifts[j])
+        # print("after:",i, j, p, s)
+        if money < 0:  
             break
         cnt += 1
     max_cnt = max(max_cnt, cnt)
+        
+    
 
 print(max_cnt)
