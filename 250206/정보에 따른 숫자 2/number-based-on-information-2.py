bf16178e64ab3acccae5d5378c28c5f4269f7399
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-06 16:23:07 +0900

    Title: 정보에 따른 숫자 2, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250206/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 250206/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,22 +1,18 @@
 ### 정보에 따른 숫자 2
 T, a, b = map(int, input().split())
-
-alpha = [0] * (1001)
-for t in range(T):
-    char, idx = input().split()
-    if char == "N":
-        alpha[int(idx)] = "N"
-    else:
-        alpha[int(idx)] = "S"
+alpha = [tuple(input().split()) for _ in range(T)]
 
 cnt = 0
 for i in range(a, b + 1):
     d1, d2 = 1000,1000
-    for j in range(1, 1001):
-        if alpha[j] == 'S':
-            d1 = min(d1, abs(j-i))
-        if alpha[j] == 'N':
-            d2 = min(d2, abs(j-i))
+    for t in range(T):
+        char, idx = input().split()
+        idx = int(idx)
+        
+        if char == 'S':
+            d1 = min(d1, abs(i-idx))
+        if char == 'N':
+            d2 = min(d2, abs(i-idx))
 
     if d1 <= d2:
         cnt += 1
