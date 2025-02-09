8401915367cfb45cb2b266068cd716e4258d6afb
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-09 23:49:29 +0900

    Title: 야바위, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250209/야바위/ya-rock.py
@@ -0,0 +1,19 @@
+### 잘 모르는 상황에서의 완전탐색 / 야바위
+n = int(input())
+moves = [tuple(map(int, input().split())) for _ in range(n)]
+a, b, c = zip(*moves)
+a, b, c = list(a), list(b), list(c)
+
+# Write your code here!
+max_cnt = 0
+for i in range(3):
+    cnt = 0
+    position = [0, 0, 0]
+    position[i] = 1
+    for a, b, c in moves:
+        position[a - 1], position[b - 1] = position[b - 1], position[a - 1]
+        if position[c - 1] == 1:
+            cnt += 1
+        
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
