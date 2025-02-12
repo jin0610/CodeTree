f0fe81238f2005884140d13e3a29774943a11869
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-12 21:51:34 +0900

    Title: 팀으로 하는 틱택토 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250212/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250212/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -3,10 +3,11 @@ inp = [list(map(int, list(str(input())))) for _ in range(3)]
 
 # Write your code here!
 answer = 0
+check_list = []
 # 가로 
 for i in range(3):
     if len(set(inp[i])) == 2:
-        answer += 1
+        check_list.append(tuple(inp[i]))
 
 # 세로
 for i in range(3):
@@ -14,7 +15,7 @@ for i in range(3):
     for j in range(3):
         arr_1.add(inp[j][i])
     if len(set(arr_1)) == 2:
-        answer+=1
+        check_list.append(tuple(arr_1))
 
 # 대각선
 arr_1 = set()
@@ -24,11 +25,11 @@ for i in range(3):
     arr_2.add(inp[i][2-i])
 
 if len(arr_1) == 2:
-    answer+=1
+    check_list.append(tuple(arr_1))
 if len(arr_2) == 2:
-    answer+=1
+    check_list.append(tuple(arr_2))
 
-print(answer)
+print(len(set(check_list)))
         
 
         
