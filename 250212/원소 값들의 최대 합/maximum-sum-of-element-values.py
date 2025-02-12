8f6b6c7599b2562fa99f6b9852dda1cdab15842e
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-12 23:59:51 +0900

    Title: 원소 값들의 최대 합, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250212/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 250212/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -3,11 +3,11 @@ arr = [0] + list(map(int, input().split()))
 
 # Write your code here!
 answer = 0
-for i in range(n+1):
-    temp = [0] * (n+1)
+for i in range(n + 1):
+    temp = 0
     idx = i
     for j in range(m):
-        temp[arr[idx]] = arr[idx]
+        temp += arr[idx]
         idx = arr[idx]
     answer = max(answer, sum(temp))
 print(answer)
