be3940bc10891db0205ae40fe929a9341e90ebc1
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-16 21:12:14 +0900

    Title: 독서실의 거리두기 4, Time: 64ms, Memory: 17MB, Status: Passed - CodeTree

--- 250216/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 250216/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -5,21 +5,24 @@ seat = list(map(int, list(input())))
 # Write your code here!
 answer = 0
 for i in range(N-1):
-    for j in range(i + 1, N):
-        temp = seat.copy()
-        if seat[i] == 1 or seat[j] == 1:
+    if seat[i] == 1:
+        continue
+    for j in range(i+1, N):
+        if seat[j] == 1:
             continue
-        temp[i] = 1
-        temp[j] = 1
-        idx = []
-        dist = 1000
-        for k in range(N):
-            if seat[k] == 1:
-                idx.append(k)
 
-        for k in range(len(idx)-1):
-            dist = min(dist, abs(idx[k+1] - idx[k]))
-
+        flag = False
+        idx = 0
+        dist = 101
+        for k in range(N):    
+            
+            if (seat[k] == 1 or i == k or j == k):
+                if flag == False:
+                    flag = True
+                    idx = k
+                else:
+                    dist = min(dist, abs(idx - k))
+                    idx = k
         answer = max(answer, dist)
 
-print(answer-1)
+print(answer)
