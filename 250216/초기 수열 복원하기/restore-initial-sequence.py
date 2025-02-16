d6a542cc22a272931cb0a405055afcaf31fb79a8
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-16 21:46:39 +0900

    Title: 초기 수열 복원하기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250216/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,23 @@
+### 완전탐색 / 초기 수열 복원하기
+N = int(input())
+arr = list(map(int, input().split()))
+
+answer = [0] * N
+for i in range(N - 1):
+    answer[i] = (i + 1)
+    answer[i + 1] = arr[i] - answer[i] 
+
+for i in range(1, N + 1):
+    answer[0] = i
+    for j in range(N-1):
+        answer[j + 1] = arr[j] - answer[j]
+        if (answer[j + 1] < 0) or (answer[j + 1] > N):
+            break
+    
+    if len(set(answer)) == N:
+        print(" ".join(map(str,answer)))
+        break
+    else:
+        answer = [0] * N
+
+    
