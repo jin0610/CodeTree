ef0c144b054e95e48c753dd76d14babaca58d170
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-16 21:59:49 +0900

    Title: 초기 수열 복원하기, Time: 52ms, Memory: 17MB, Status: Passed - CodeTree

--- 250216/초기 수열 복원하기/restore-initial-sequence.py
+++ 250216/초기 수열 복원하기/restore-initial-sequence.py
@@ -2,22 +2,20 @@
 N = int(input())
 arr = list(map(int, input().split()))
 
-answer = [0] * N
-for i in range(N - 1):
-    answer[i] = (i + 1)
-    answer[i + 1] = arr[i] - answer[i] 
-
 for i in range(1, N + 1):
+    answer = [0] * N
+    flag = True
     answer[0] = i
     for j in range(N-1):
-        answer[j + 1] = arr[j] - answer[j]
-        if (answer[j + 1] <= 0) or (answer[j + 1] > N):
+        num = arr[j] - answer[j]
+        if (num <= 0) or (num > N) or (num in answer):
+            flag = False
             break
+        answer[j + 1] = num
     
-    if len(set(answer)) == N:
+    if flag:
         print(" ".join(map(str,answer)))
         break
-    else:
-        answer = [0] * N
+        
 
     
