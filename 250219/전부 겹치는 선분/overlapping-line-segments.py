07ec9dcfc45abaa0f890b44382f047255bbe4b68
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-19 21:00:26 +0900

    Title: 전부 겹치는 선분, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250219/전부 겹치는 선분/overlapping-line-segments.py
+++ 250219/전부 겹치는 선분/overlapping-line-segments.py
@@ -1,22 +1,39 @@
 ### 케이스 별로 나누기 / 전부 겹치는 선분
+
+### 풀이 2.
 N = int(input())
-points = [map(int,input().split()) for _ in range(N)]
 
-def intersecting(x1, x2, a1, a2):
-    if x1 > a2 or x2 < a1:
-        return False
-    else:
-        return True
+max_x1 = 0
+min_x2 = 100
+for i in range(N):
+    x1, x2 = map(int, input().split())
+    max_x1 = max(max_x1, x1)
+    min_x2 = min(min_x2, x2)
 
-answer = "Yes"
-x1, x2 = points[0]
-for i in range(1, N):
-    a1, a2 = points[i]
-    if intersecting(x1, x2, a1, a2):
-        x1, x2 = max(x1, a1), min(x2, a2)
-    else:
-        answer = "No"
-        break
+if max_x1 <= min_x2:
+    print("Yes")
+else:
+    print("No")
+
+### 풀이 1, 선분이 겹칠 경우의 선 찾기
+# N = int(input())
+# points = [map(int,input().split()) for _ in range(N)]
+
+# def intersecting(x1, x2, a1, a2):
+#     if x1 > a2 or x2 < a1:
+#         return False
+#     else:
+#         return True
+
+# answer = "Yes"
+# x1, x2 = points[0]
+# for i in range(1, N):
+#     a1, a2 = points[i]
+#     if intersecting(x1, x2, a1, a2):
+#         x1, x2 = max(x1, a1), min(x2, a2)
+#     else:
+#         answer = "No"
+#         break
 
 
-print(answer)
+# print(answer)
