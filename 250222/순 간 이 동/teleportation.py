741e95d2128cc955c9cceb05fa50a96d08b68471
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-22 22:00:03 +0900

    Title: 순 간 이 동, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250222/순 간 이 동/teleportation.py
+++ 250222/순 간 이 동/teleportation.py
@@ -1,7 +1,7 @@
 # 최선의 가짓수 따지기 / 순간이동
 A, B, x, y = map(int, input().split())
 
-dist1 = B - A                       # A -> B로 간 거리
+dist1 = abs(B - A)                       # A -> B로 간 거리
 dist2 = abs(A - x) + abs(B - y)     # A -> x -순간이동- y -> B
 dist3 = abs(A - y) + abs(B - x)     # A -> y -순간이동- x -> B
 print(min(dist1, dist2, dist3))
