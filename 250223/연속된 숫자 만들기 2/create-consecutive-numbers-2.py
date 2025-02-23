8956f62ef21b2d8bb80ffd57e6ebc7e5f05df25f
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-23 22:09:56 +0900

    Title: 연속된 숫자 만들기 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250223/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250223/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,5 +1,5 @@
 ### 케이스 별로 나누기(상황에 따른 최선의 움직임) / 연속된 숫자 만들기 2
-A, B, C = map(int, input().split())
+A, B, C = sorted(map(int, input().split()))
 
 if abs(B - A) == 1 and abs(C - B) == 1:
     print(0)
