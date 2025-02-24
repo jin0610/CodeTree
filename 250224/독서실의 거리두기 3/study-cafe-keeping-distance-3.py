d82b9af3e9056dd94afc61b08d74a5be6cb74b0f
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-24 23:48:17 +0900

    Title: 독서실의 거리두기 3, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250224/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 250224/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -3,16 +3,21 @@ N = int(input())
 seats = list(input())
 
 # Write your code here!
+# step1. '1'이 놓인 index 번호 찾기
 seats_idx = []
 for i in range(N):
     if seats[i] == "1":
         seats_idx.append(i)
 
+# step2.인접한 쌍들 중 가장 먼 1간의 길이 찾기
 max_dist = 0
 for i in range(len(seats_idx)-1):
     max_dist = max(max_dist, seats_idx[i + 1] - seats_idx[i])
 
+# step3. 가장 먼 쌍의 위치 가운데에 '1' 놓기
 answer = max_dist // 2
+
+# step4. 인접한 쌍들 중 가장 가까운 1간의 길이 찾기
 for i in range(len(seats_idx) - 1):
     answer = min(answer, seats_idx[i+1] - seats_idx[i])
 
