17d635115cd94687af8e5e54fbd0988fb986a484
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-25 23:19:44 +0900

    Title: 연속된 숫자 만들기 3, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250225/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 250225/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,7 +1,11 @@
 ### 케이스 별로 나누기 - 좋은 전략 추려내기 / 연속된 숫자 만들기 3
 a = list(map(int, input().split()))
 
+# Step 1. list 정렬
 a = sorted(a)
 
+# Step 2. 인접한 두 숫자의 간격을 중 더 긴 간격 구하기
 max_diff = max(a[2]-a[1], a[1] - a[0])
+
+# Step 3. 더 긴 간격의 -1을 하여 정답 출력
 print(max_diff-1)
