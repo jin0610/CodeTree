6ac57a75c54b0ed2285dbf2e372453b4cfa08a19
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-02-27 17:46:52 +0900

    Title: 순위 경쟁2, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/순위 경쟁2/ranking-competition2.py
+++ 250227/순위 경쟁2/ranking-competition2.py
@@ -1,7 +1,7 @@
 ### 케이스 별로 나누기(가능한 상황을 나열하기) - 순위 경쟁2
 N = int(input())
 
-honor = ""
+honor = "AB"
 changeCnt = 0
 A_score = 0
 B_score = 0
@@ -13,10 +13,6 @@ for i in range(N):
         A_score += s
     else:
         B_score += s
-
-    if i == 0:
-        honor = 'AB'
-        continue
 
     max_score = max(A_score, B_score)
     if max_score == A_score and max_score == B_score:
