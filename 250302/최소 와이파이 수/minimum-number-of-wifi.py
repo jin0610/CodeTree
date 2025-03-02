c7aeba98fc0793e0d3235b6e83bc30d2ac2012c7
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-03-02 23:30:07 +0900

    Title: 최소 와이파이 수, Time: 63ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250302/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,28 @@
+### Ad-hoc (지극히 최선인 전략히 확실해진 경우) / 최소 와이파이 수
+import sys
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# 와이파이를 사용 가능한 거리
+use_dist = 2 * m + 1
+
+# n이 use_dist보다 작거나 같은 경우
+if n <= use_dist:
+    print(1)
+    sys.exit(0)
+
+# n이 use_dist보다 작거나 같은 경우
+idx = arr.index(1)
+cnt = 0
+while True:
+    idx += use_dist
+    cnt += 1
+
+    if idx >= n:
+        break
+
+    if arr[idx] == 0:
+        idx += 1
+
+    
+print(cnt)
