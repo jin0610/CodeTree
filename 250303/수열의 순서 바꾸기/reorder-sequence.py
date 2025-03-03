1574cf3f27924944779635601e13ca4617d11bbf
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-03-03 23:15:23 +0900

    Title: 수열의 순서 바꾸기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250303/수열의 순서 바꾸기/reorder-sequence.py
@@ -0,0 +1,14 @@
+### Ad-hoc (지극히 최선인 전략이 확실히 정해지는 경우) / 수열의 순서 바꾸기
+n = int(input())
+sequence = list(map(int, input().split()))
+
+# Please write your code here.
+if n == 1:
+    print(0)
+else:
+    answer = 0
+    for i in range(n-1):
+        if sequence[i] > sequence[i + 1]:
+            answer = i + 1
+            break
+    print(answer)
