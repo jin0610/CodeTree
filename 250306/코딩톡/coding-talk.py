302b9d2dcb346a5b1539a075c6e04db560edbe71
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-03-06 21:50:13 +0900

    Title: 코딩톡, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250306/코딩톡/coding-talk.py
+++ 250306/코딩톡/coding-talk.py
@@ -8,14 +8,20 @@ person = []
 for i in range(n):
     person.append(chr(ord('A') + i))
 
-# 만약 message를 안본 수가 0이라면 공백 출력
+# Case 1. 만약 message를 안본 수가 0이라면 공백 출력
 if messages[p-1][1] == '0':
     print()
     sys.exit(0)
 
-# 메시지를 쓴 시점 이후 메시지를 쓴 사람들 제외(메시지를 쓴 사람도 제외)
+# Case 2. 메시지를 쓴 시점 이후 메시지를 쓴 사람들 제외(메시지를 쓴 사람도 제외)
 for i in range(p-1, m):
     if messages[i][0] in person:
         person.remove(messages[i][0])
 
+# Case 3. p 이전의 메시지를 안본 수(n1) = p번째 메시지를 안본 수(n2) ====> n2와 같은 수는 모두 제외
+m_cnt = messages[p-1][1]
+for i in range(p - 1):
+    if messages[i][1] == m_cnt and messages[i][0] in person:
+        person.remove(messages[i][0])
+        
 print(' '.join(person))
