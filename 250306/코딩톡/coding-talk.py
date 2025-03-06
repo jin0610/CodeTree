10fc675d272d8f919060091c07255e8073297eac
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-03-06 21:35:49 +0900

    Title: 코딩톡, Time: 39ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250306/코딩톡/coding-talk.py
+++ 250306/코딩톡/coding-talk.py
@@ -1,17 +1,21 @@
 ### 케이스별로 나누기 (일어난 상황에 대한 추론) / 코딩
+import sys
 n, m, p = map(int, input().split())
-messages = [tuple(input().split()) for _ in range(m)]
-c = [msg[0] for msg in messages]
-u = [int(msg[1]) for msg in messages]
+messages = [list(input().split()) for _ in range(m)]
 
 # 채팅방에 들어와 있는 사람들
 person = []
 for i in range(n):
     person.append(chr(ord('A') + i))
 
+# 만약 message를 안본 수가 0이라면 공백 출력
+if messages[p-1][1] == '0':
+    print()
+    sys.exit(0)
+
 # 메시지를 쓴 시점 이후 메시지를 쓴 사람들 제외(메시지를 쓴 사람도 제외)
-for i in range(p-1, n):
-    if c[i] in person:
-        person.remove(c[i])
+for i in range(p-1, m):
+    if messages[i][0] in person:
+        person.remove(messages[i][0])
 
 print(' '.join(person))
