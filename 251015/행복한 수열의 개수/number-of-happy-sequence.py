350df49ac1ce6957a973a109f38192e0eb023553
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-15 17:44:49 +0900

    Title: 행복한 수열의 개수, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251015/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,38 @@
+# 입력
+N, M = map(int, input().split(' '))
+arr = [
+    list(map(int,input().split(' ')))
+    for _ in range(N)
+]
+
+# 행복한 수열 찾기
+result = 0
+
+# 열
+for i in range(N):
+    cnt = 1
+    for j in range(N-1):
+        if arr[i][j] == arr[i][j+1]:
+            cnt = cnt + 1
+        else:
+            cnt = 1
+
+        if cnt >= M:
+                result = result + 1
+                break
+
+        
+
+# 행
+for i in range(N):
+    cnt = 1
+    for j in range(N-1):
+        if arr[j][i] == arr[j+1][i]:
+            cnt = cnt + 1
+        else:
+            cnt = 1
+
+        if cnt >= M:
+                result = result + 1
+                break
+print(result)
