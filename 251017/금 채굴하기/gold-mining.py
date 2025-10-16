b99f4a6febab78580fd149e8798257d06835724d
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-17 03:34:45 +0900

    Title: 금 채굴하기, Time: 68ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 251017/금 채굴하기/gold-mining.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+def in_range(x, y):
+    if x >= 0 and x < N and y >= 0 and y < N:
+        return True
+    return False
+
+# 0 <= K <= N
+result = 0 # 금의 개수
+for i in range(N):
+    for j in range(N):
+        for k in range(N+1):
+            cost = k ** 2 + (k + 1) ** 2
+            gold = 0 # 금의 개수
+            
+            for x in range(N):
+                for y in range(N):
+                    if abs(i - x) + abs(j - y) <= k:
+                        gold += grid[x][y]
+            if cost <= M * gold:
+                result = max(result, gold)
+
+print(result)
