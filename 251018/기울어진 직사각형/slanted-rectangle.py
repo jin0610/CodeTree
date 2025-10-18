8aa0c17c023ed0cca46e34bd3259adf05bcee53b
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-18 17:37:05 +0900

    Title: 기울어진 직사각형, Time: 133ms, Memory: 21MB, Status: Passed - Codetree

--- 251018/기울어진 직사각형/slanted-rectangle.py
+++ 251018/기울어진 직사각형/slanted-rectangle.py
@@ -1,36 +1,36 @@
+N = int(input())
+grid = [
+    list(map(int,input().split()))
+    for _ in range(N)
+]
+
 def in_range(x, y):
     return x >= 0 and x < N and y >= 0 and y < N
 
-def calculate(x, y, w, h):
-    move_cnt = [w, h, w, h]
-    num = 0
-    for dx, dy, length in zip(dxs, dys, move_cnt):
+def get_score(x, y, w, h):
+    dxs = [1, -1, -1, 1]
+    dys = [-1, -1, 1, 1]
+    move = [w, h, w, h]
+
+    num_sum = 0
+    for dx, dy, length in zip(dxs, dys, move):
         for _ in range(length):
             x += dx
             y += dy
-
+            
             if not in_range(x, y):
                 return 0
+
+            num_sum += grid[y][x]
+
+    return num_sum
+
+answer = 0
+for i in range(N):
+    for j in range(N):
+        for w in range(1, N):
+            for h in range(1, N):
+                answer = max(answer, get_score(j, i, w, h))
+
+print(answer)
             
-            num += grid[y][x]
-
-    return num
-        
-if __name__ == '__main__':
-    N = int(input())
-    grid = [
-        list(map(int,input().split()))
-        for _ in range(N)
-    ]
-
-    dxs = [1, -1, -1, 1]
-    dys = [-1, -1, 1, 1]
-    answer = 0
-
-    for i in range(N):
-        for j in range(N):
-            for w in range(1, N):
-                for h in range(1, N):
-                    answer = max(answer, calculate(j, i, w, h))
-
-    print(answer)
