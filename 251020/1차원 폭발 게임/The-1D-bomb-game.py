c1c6433a648570031d5753b9ed7c7239318d5c5d
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-20 01:50:08 +0900

    Title: 1차원 폭발 게임, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251020/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,50 @@
+N, M = map(int,input().split())
+blocks = [
+    int(input())
+    for _ in range(N)
+]
+
+def isSequence():
+    global blocks, M
+    for i in range(len(blocks)-1):
+        cnt = 1
+        for j in range(i + 1, len(blocks)):
+            if blocks[i] != blocks[j]:
+                break
+            cnt += 1
+        if cnt >= M:
+            return True
+
+    return False
+
+def remove_num():
+    global blocks, M
+    temp, idx, temp_idx = [], [], []
+    for i in range(len(blocks)-1):
+        cnt = 1
+        temp_idx.append(i)
+        for j in range(i + 1, len(blocks)):
+            if blocks[i] != blocks[j]:
+                break
+            cnt += 1
+            temp_idx.append(j)
+        if cnt >= M:
+            idx = idx + temp_idx
+        temp_idx = []
+
+    for i in range(len(blocks)):
+        if i not in idx:
+            temp.append(blocks[i])
+
+    blocks = temp[:]
+
+while True:
+    if not isSequence():
+        break
+    remove_num()
+
+print(len(blocks))
+if len(blocks) > 0:
+    for n in blocks:
+        print(n)
+    
