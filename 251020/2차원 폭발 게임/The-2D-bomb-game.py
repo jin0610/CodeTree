6c66f1a976c9b68dc36df2f1dd5943a43af47f57
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-20 16:23:44 +0900

    Title: 2차원 폭발 게임, Time: 2078ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 251020/2차원 폭발 게임/The-2D-bomb-game.py
+++ 251020/2차원 폭발 게임/The-2D-bomb-game.py
@@ -1,88 +1,86 @@
-import sys
 N, M, K = map(int, input().split())
-numbers_2d = [list(map(int, input().split())) for _ in range(N)]
+grid = [
+    list(map(int,input().split()))
+    for _ in range(N)
+]
 
-# 연속으로 반복된 부분이 있는 지 확인
-def is_bomb():
-    global numbers_2d
-    current_row_idx, end_row_idx = 0, 0
-    for r in range(N):
-        for c in range(N):
-            current_row_idx = r
-            if numbers_2d[r][c] == 0:
-                continue
-            end_row_idx = find_bomb_end_row(current_row_idx, c, numbers_2d[r][c])
-            if end_row_idx - current_row_idx + 1 >= M:
-                return True
+# 연속된 부분의 끝점 확인
+def get_bomb_end_idx(curr_row, col, num):
+    global grid
+    for row in range(curr_row + 1, N):
+        if grid[row][col] != num:
+            return row - 1
+    return N - 1
 
-    if end_row_idx - current_row_idx + 1 >= M:
-        return True
-    return False
+# 연속된 부분이 있는지 확인
+# def is_bomb_possible():
+#     global grid
+    
+#     for r in range(N):
+#         for c in range(N):
+#             if grid[r][c] == 0:
+#                 continue
+#             if 
 
-# 연속한 부분의 끝 점 찾기
-def find_bomb_end_row(r, c, num):
-    for end_row in range(r + 1, N):
-        if numbers_2d[end_row][c] != num:
-            return end_row - 1
-    return len(numbers_2d[r]) - 1
-
-# 폭탄 터지기
+# 폭발
 def bomb():
-    for c in range(N):
-        for r in range(N):
-            if numbers_2d[r][c] == 0:
-                continue
-            end_row = find_bomb_end_row(r, c, numbers_2d[r][c])
-            if end_row - r + 1 >= M:
-                for row in range(r, end_row + 1):
-                    numbers_2d[row][c] = 0
-    gravity()
+    while True:
+        did_explode = False
 
-# 회전하기
+        for c in range(N):
+            for curr_row in range(N):
+                if grid[curr_row][c] == 0:
+                    continue
+                
+                end_row = get_bomb_end_idx(curr_row, c, grid[curr_row][c])
+                if end_row - curr_row + 1 >= M:
+                    for row in range(curr_row, end_row + 1):
+                        grid[row][c] = 0
+                    did_explode = True
+        gravity()
+        if not did_explode:
+            return
+        
+# 회전
 def rotation():
-    global numbers_2d
+    global grid
     temp = [
         [ 0 for _ in range(N)]
         for _ in range(N)
     ]
+
     for r in range(N):
         for c in range(N):
-            temp[c][N - r - 1] = numbers_2d[r][c]
+            temp[c][N - r - 1] = grid[r][c]
 
-    numbers_2d = [
-        temp[i][:] for i in range(N)
-    ]
+    grid = [temp[r][:] for r in range(N)]
+
     gravity()
 
 # 중력
 def gravity():
-    global numbers_2d
+    global grid
     for c in range(N):
         for r in range(N - 1, 0, -1):
-            if numbers_2d[r][c] == 0:
+            if grid[r][c] == 0:
                 nr = r - 1
                 while nr >= 0:
-                    if numbers_2d[nr][c] != 0:
-                        numbers_2d[r][c] = numbers_2d[nr][c]
-                        numbers_2d[nr][c] = 0
+                    if grid[nr][c] != 0:
+                        grid[r][c] = grid[nr][c]
+                        grid[nr][c] = 0
                         break
                     else:
                         nr -= 1
-if M == 1:
-    print(0)
-    sys.exit(0)
 
 for _ in range(K):
-    while is_bomb():
-        bomb()
-    rotation()
-        
-while is_bomb():
     bomb()
+    rotation()
+bomb()
 
 cnt = 0
 for i in range(N):
     for j in range(N):
-        if numbers_2d[i][j] != 0:
+        if grid[i][j] != 0:
             cnt += 1
 print(cnt)
+
