f6e1c2abb322121993d27918fb250d905a381241
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-20 11:02:32 +0900

    Title: 1차원 폭발 게임, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251020/1차원 폭발 게임/The-1D-bomb-game.py
+++ 251020/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,53 +1,38 @@
-import sys
-N, M = map(int,input().split())
-blocks = [
+n, m = map(int,input().split())
+numbers = [
     int(input())
-    for _ in range(N)
+    for _ in range(n)
 ]
 
-if M == 1:
-    print(0)
-    sys.exit()
-def isSequence():
-    global blocks, M
-    for i in range(len(blocks)-1):
-        cnt = 1
-        for j in range(i + 1, len(blocks)):
-            if blocks[i] != blocks[j]:
-                break
-            cnt += 1
-        if cnt >= M:
-            return True
-    return False
-
-def remove_num():
-    global blocks, M
-    temp, idx, temp_idx = [], [], []
-    for i in range(len(blocks)-1):
-        cnt = 1
-        temp_idx.append(i)
-        for j in range(i + 1, len(blocks)):
-            if blocks[i] != blocks[j]:
-                break
-            cnt += 1
-            temp_idx.append(j)
-        if cnt >= M:
-            idx = idx + temp_idx
-        temp_idx = []
-
-    for i in range(len(blocks)):
-        if i not in idx:
-            temp.append(blocks[i])
-
-    blocks = temp[:]
+# 주어진 시작점에 대하여 부분 수열의 끝 위치 반환
+def get_end_idx_of_explosion(start_idx, curr_num):
+    for end_idx in range(start_idx + 1, len(numbers)):
+        if numbers[end_idx] != curr_num:
+            return end_idx - 1
+    
+    return len(numbers) - 1
 
 while True:
-    if not isSequence():
-        break
-    remove_num()
+    did_explode = False
 
-print(len(blocks))
-if len(blocks) > 0:
-    for n in blocks:
-        print(n)
-    
+    for curr_idx, number in enumerate(numbers):
+        # 각 위치마다 그 뒤로 폭탄이 m개 이상있는지 확인
+        # 이미 터지기로 예정된 폭탄은 패스
+        if number == 0:
+            continue
+        
+        # curr_idx 부터 연속하여 같은 숫자를 갖는 폭탄 중 가장 마지막 위치 찾아 반환
+        end_idx = get_end_idx_of_explosion(curr_idx, number)
+
+        if end_idx-curr_idx+1 >= m:
+            numbers[curr_idx:end_idx+1] = [0]  * (end_idx - curr_idx + 1)
+            did_explode = True
+
+    # 폭탄이 터진 이후의 결과를 계산하여 반영해줍니다.
+    numbers = list(filter(lambda x: x > 0, numbers))
+    if not did_explode:
+        break
+
+print(len(numbers))
+for number in numbers:
+    print(number)
