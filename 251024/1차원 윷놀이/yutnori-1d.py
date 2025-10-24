71e3eca4230a069d04cac6ae7b9cd35feefae6b4
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-24 17:38:46 +0900

    Title: 1차원 윷놀이, Time: 3304ms, Memory: 24MB, Status: Passed - Codetree

--- 251024/1차원 윷놀이/yutnori-1d.py
+++ 251024/1차원 윷놀이/yutnori-1d.py
@@ -2,17 +2,14 @@ N, M, K = map(int, input().split())     # 턴의 수(N), 윷놀이 판의 상태
 lengths = list(map(int, input().split()))
 
 def cal_score():
-    global numbers
-    move = {}
+    global numbers, lengths
+    move_status = [1] * K # [1. 1. 1. 1]
     for i in range(len(numbers)):
-        if move.get(numbers[i]):
-            move[numbers[i]] += (lengths[i])
-        else:
-            move[numbers[i]] = 1 + (lengths[i])
-    
+        move_status[numbers[i] - 1] += lengths[i]
+
     score = 0
-    for key, value in move.items():
-        if value >= M:
+    for status in move_status:
+        if status >= M:
             score += 1
     return score
 
@@ -21,7 +18,6 @@ result = 0
 def backtracking(depth, cnt):
     global numbers, result
     if depth == cnt:
-        # print(numbers)
         result = max(result, cal_score())
         return
 
