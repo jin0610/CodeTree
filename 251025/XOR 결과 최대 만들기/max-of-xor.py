5777fe135e9020a28e10678d621fe61ddacb8657
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-25 15:05:46 +0900

    Title: XOR 결과 최대 만들기, Time: 82ms, Memory: 19MB, Status: Passed - Codetree

--- 251025/XOR 결과 최대 만들기/max-of-xor.py
+++ 251025/XOR 결과 최대 만들기/max-of-xor.py
@@ -1,18 +1,23 @@
-N, M = map(int, input().split())
-arr = list(map(int, input().split()))
+N, M = map(int, input().split()) # N개의 정수, 뽑을 숫자 갯수 M
+nums = list(map(int, input().split()))
 
 answer = 0
-def find_max_num(idx, curr_num, cnt):
+def get_max_xor(curr_idx, cnt, curr_num):
     global answer
 
-    if cnt >= M:
+    if cnt == M: # M번 함수가 실행되어 숫자가 뽑혔을 경우, 함수 종료
         answer = max(answer, curr_num)
         return
 
-    for i in range(idx, N):
-        find_max_num(i + 1, curr_num ^ arr[i], cnt + 1)
-
+    # 현재 인덱스가 N보다 크거나 같을 경우 멈추기
+    # 현재 남아있는 숫자의 갯수가 부족할 경우 멈추기
+    if curr_idx >= N or N - cnt < M - cnt:
+        return
+    
+    for n in range(curr_idx, N):
+        get_max_xor(n + 1, cnt + 1, curr_num ^ nums[n])
+    # get_max_xor(curr_idx + 1, cnt + 1, curr_num ^ arr[curr_idx])
     return
 
-find_max_num(0, 0, 0)
+get_max_xor(0, 0, 0)
 print(answer)
