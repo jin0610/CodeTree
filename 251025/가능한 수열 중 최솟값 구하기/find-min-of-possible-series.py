62786d510b5cb929fb22c70e589810cb20a2b4cf
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-25 10:16:45 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 1093ms, Memory: 23MB, Status: Time Limit Exceed - Codetree

--- 251025/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 251025/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -2,9 +2,11 @@ import sys
 n = int(input())
 
 def is_possible(numbers):
-    for length in range(1, n // 2 + 1):
+    for length in range(2, n // 2 + 1):
         for i in range(n - 2 * length + 1):
-            if numbers[i : i + length] == numbers[i + length : i + 2*length]:
+            list_1 = numbers[i : i + length]
+            list_2 = numbers[i + length : i + 2 * length]
+            if list_1 == list_2:
                 return False
 
     return True
@@ -16,10 +18,12 @@ def backtracking(depth, cnt):
     if depth == cnt:
         if is_possible(numbers):
             print("".join(map(str,numbers)))
-            sys.exit()
+            sys.exit(0)
         return
 
     for i in range(4, 7):
+        if numbers and numbers[-1] == i:
+            continue
         numbers.append(i)
         backtracking(depth, cnt + 1)
         numbers.pop()
