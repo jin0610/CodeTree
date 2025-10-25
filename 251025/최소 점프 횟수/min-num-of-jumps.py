d1249e5acf18b6ae0b3659d97cd3dc8bfd5e04e3
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-25 13:01:16 +0900

    Title: 최소 점프 횟수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251025/최소 점프 횟수/min-num-of-jumps.py
+++ 251025/최소 점프 횟수/min-num-of-jumps.py
@@ -7,7 +7,6 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 answer = _MAX
-
 def backtracking(idx, cnt):
     global answer
 
@@ -15,12 +14,12 @@ def backtracking(idx, cnt):
         answer = min(answer, cnt)
         return
 
-    for dist in range(1, num[idx] + 1):
-        backtriking(idx + dist, cnt + 1)
+    for dist in range(1, nums[idx] + 1):
+        backtracking(idx + dist, cnt + 1)
 
 backtracking(0, 0)
 
-if answer = _MAX:
+if answer == _MAX:
     answer = -1
 
 print(answer)
