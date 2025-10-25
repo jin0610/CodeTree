7ffa9041861d6bf2b1ed97bb2c2fdd5323d31d37
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-25 19:13:43 +0900

    Title: 외판원 순회, Time: 253ms, Memory: 23MB, Status: Passed - Codetree

--- 251025/외판원 순회/traveling-salesman-problem.py
+++ 251025/외판원 순회/traveling-salesman-problem.py
@@ -6,41 +6,39 @@ A = [
 ]
 visited = [False] * N
 visited[0] = True
-def calc_cost():
-    global points
+def calc_cost(channel):
 
     cost = 0
     pre_point = 0
-    for p in points:
-        if A[pre_point][p] == 0:
-            return  sys.maxsize
-        cost += A[pre_point][p]
-        pre_point = p
-    cost += A[pre_point][0]
+    for point in channel:
+        cost += A[pre_point][point]
+        pre_point = point
+
     return cost
 
-points = []
+points = [0]
 answer = sys.maxsize
-def choose_point(curr_num):
+def choose_point(curr_num, last):
     global visited, points, answer
 
     if curr_num == N:
-        answer = min(answer, calc_cost())
+        if A[last][0] != 0:
+            answer = min(answer, calc_cost(points + [0]))
         return
 
     for n in range(1, N):
-        if visited[n]:
+        if visited[n] or A[last][n] == 0:
             continue
         
         points.append(n)
         visited[n] = True
         
-        choose_point(curr_num + 1)
+        choose_point(curr_num + 1, n)
 
         points.pop()
         visited[n] = False
 
     return
 
-choose_point(1)
+choose_point(1, 0)
 print(answer)
