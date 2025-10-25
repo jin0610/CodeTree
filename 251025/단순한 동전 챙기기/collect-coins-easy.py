180cd6bf901e8b1d9cbe8e3df3038ff687502e94
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-25 16:41:30 +0900

    Title: 단순한 동전 챙기기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251025/단순한 동전 챙기기/collect-coins-easy.py
@@ -0,0 +1,71 @@
+import sys
+INT_MAX = sys.maxsize
+n = int(input())
+grid = [list(input()) for _ in range(n)]
+
+# 시작점, 끝점, 동전 찾기
+coins_pos = {}
+coins = []
+for r in range(n):
+    for c in range(n):
+        if grid[r][c] == 'S':
+            start = (r, c)
+        elif grid[r][c] == 'E':
+            end = (r, c)
+        elif grid[r][c] == '.':
+            continue
+        else:
+            coins_pos[grid[r][c]] = (r, c)
+            coins.append(grid[r][c])
+coins = sorted(coins)
+
+# 최소 3개의 동전을 수집해 도착지에 도달하기 위해 필요한 최소한 이동 횟수
+def get_dist(coins):
+    global coins_pos, start, end
+    dist = 0
+    # start -> coins[0]
+    coin1 = coins_pos[coins[0]]
+    dist += (abs(start[0] - coin1[0]) + abs(start[1] - coin1[1]))
+
+    # coins간 이동
+    for i in range(2):
+        coin1 = coins_pos[coins[i]]
+        coin2 = coins_pos[coins[i + 1]]
+
+        dist += (abs(coin1[0] - coin2[0]) + abs(coin1[1] - coin2[1]))
+
+    # coins -> end
+    coin1 = coins_pos[coins[-1]]
+    dist += (abs(end[0] - coin1[0]) + abs(end[1] - coin1[1]))
+
+    return dist
+
+answer = INT_MAX
+coin_comb = []
+def get_min_move(curr_idx, cnt):
+    global answer, coins, coin_comb
+
+    # 종료조건 : 3개의 동전 고르기
+    if cnt == 3:
+        answer = min(answer, get_dist(coin_comb))
+        return
+
+    # 동전이 모자란 경우 
+    # 1. 격자 안에 동전의 갯수가 3개 미만인 경우 len(coins) < 3
+    # 2. 선택할 수 있는 남아있는 동전의 갯수 (len(coins) - curr_idx - 1)보다 선택해야하는 수가 더 큰 경우(3-cnt)
+    if len(coins) < 3 or len(coins) - curr_idx - 1 < 3 - cnt:
+        return
+
+    for i in range(curr_idx, len(coins)):
+        coin_comb.append(coins[i])
+        get_min_move(i + 1, cnt + 1)
+        coin_comb.pop()
+
+    return
+
+get_min_move(0, 0)
+
+if answer == INT_MAX:
+    print(-1)
+else:
+    print(answer)
