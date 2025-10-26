c05ac22bbdf3d19848e1a290c989c36668ca0824
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-26 15:49:11 +0900

    Title: 그래프 탐색, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251026/그래프 탐색/graph-traversal.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+edgs = [
+    [0 for _ in range(N)]
+    for _ in range(N)
+]
+for _ in range(M):
+    x, y = map(int,input().split())
+    edgs[x - 1][y - 1], edgs[y - 1][x - 1] = 1, 1
+
+visited =  [False for _ in range(N)]
+
+answer = 0
+def dfs(curr_v):
+    global answer, visited
+
+    for next_v in range(N):
+        if edgs[curr_v][next_v] and not visited[next_v]:
+            answer += 1
+            visited[next_v] = True
+            dfs(next_v)
+
+dfs(0)
+print(answer - 1)
