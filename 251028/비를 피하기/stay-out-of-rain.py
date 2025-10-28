b333272f9d3bf6c44972253ba19f1c1b792d5aab
jin0610 <87145895+jin0610@users.noreply.github.com>
2025-10-28 11:22:08 +0900

    Title: 비를 피하기, Time: 1086ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

---
+++ 251028/비를 피하기/stay-out-of-rain.py
@@ -0,0 +1,71 @@
+from collections import deque
+
+n, h, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+answer = [ [0 for _ in range(n)] for _ in range(n)] # 사람이 비를 피할 수 있다면 최소 시간
+visited = [[False for _ in range(n)] for _ in range(n)]
+distance = [[n**2 for _ in range(n)] for _ in range(n)]
+
+# 0 : 이동 가능, 1 : 벽, 2: 사람, 3 : 비를 피할 수 있는 곳
+# 사람이 있는 곳 
+persons = []
+not_rain = []   # 비를 피할 수 있는 곳
+for x in range(n):
+    for y in range(n):
+        if grid[x][y] == 2:
+            persons.append((x, y))
+        elif grid[x][y] == 3:
+            not_rain.append((x, y))
+
+# 범위 내에 있는 지 확인
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+# 갈 수 있는 지 확인
+def can_go(x, y):
+    return in_range(x, y) and not visited[x][y] and grid[x][y] != 1
+
+# visited 방문 여부 표시하는 grid 초기화
+def visited_initalize():
+    global visited
+    for x in range(n):
+        for y in range(n):
+            visited[x][y] =  False
+            distance[x][y] = n ** 2
+
+dxs, dys = [-1, 1, 0, 0], [0,0, -1, 1]
+def bfs(x, y):
+    global visited, distance
+    # 초기화
+    visited_initalize()
+
+    q = deque([(x, y)]) # q에 사람 위치 담기
+    visited[x][y] = True  
+    distance[x][y] = 0    # 거리 0
+
+    while q:
+        cx, cy = q.popleft()
+        dist = distance[cx][cy]
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = cx + dx, cy + dy
+            if can_go(nx, ny):
+                q.append((nx, ny))
+                visited[nx][ny] = True
+                distance[nx][ny] = min(distance[nx][ny], dist + 1)
+
+for x, y in persons:
+    bfs(x, y)
+    min_dist = n**2
+    is_visited = False
+    for lx, ly in not_rain:
+        if visited[lx][ly]:
+            min_dist = min(min_dist, distance[lx][ly])
+            is_visited = True
+    if is_visited:
+        answer[x][y] = min_dist
+    else:
+        answer[x][y] =-1
+
+for x in range(n):
+    print(*answer[x])
