f2dc098240dae9509c623229b4793c1315fbbdda
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-02-17 01:37:11 +0900

    Title: 세 정수형 변수 선언, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260217/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a, b, c = 7, 23, 30;
+        System.out.printf("%d + %d = %d", a, b, c);
+    }
+}
