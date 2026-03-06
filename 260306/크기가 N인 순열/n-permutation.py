a40f62aa225d1893e56e66cd80ea1cf2d0d76942
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-06 23:27:49 +0900

    Title: 크기가 N인 순열, Time: 183ms, Memory: 25MB, Status: Passed - Codetree

---
+++ 260306/크기가 N인 순열/n-permutation.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+# Please write your code here.
+visited = [False] * (n + 1)
+comb = []
+def combs(cnt):
+    global visited, comb
+
+    if cnt == n+1:
+        print(*comb)
+        return
+
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+
+        visited[i] = True
+        comb.append(i)
+        combs(cnt + 1)
+        comb.pop()
+        visited[i] = False
+
+combs(1)
+
+
+
