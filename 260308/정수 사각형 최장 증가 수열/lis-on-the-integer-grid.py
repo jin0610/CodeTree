1b8f9be2acdb27bde69b9dc52f9cb3ee3a90c80f
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-08 02:50:13 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 222ms, Memory: 27MB, Status: Runtime Error - Codetree

---
+++ 260308/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+dp = [[-1 for _ in range(n)] for _ in range(n)]
+
+dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 0]
+
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+
+def count_dot(x, y):
+    global dp
+
+    if dp[y][x] != -1:
+        return dp[y][x]
+
+    cnt = 1
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[ny][nx] > grid[y][x]:
+            cnt = max(cnt, 1 + count_dot(nx, ny))
+    
+    dp[y][x] = cnt
+    return cnt
+    
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == -1:
+            count_dot(j, i) 
+
+answer = 0        
+for i in range(n):
+    answer = max(answer, max(dp[i]))
+
+print(answer)
