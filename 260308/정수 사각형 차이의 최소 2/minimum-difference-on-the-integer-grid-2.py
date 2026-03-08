7f99cc9329c9186c4c14705738b21c0f1f5b5f7d
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-08 15:25:54 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 64ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 260308/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 260308/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -2,12 +2,14 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-dp = [[(101, 0) for _ in range(n)] for _ in range(n)]
+dp = [[(101, 0, 101) for _ in range(n)] for _ in range(n)]
 
-dp[0][0] = (grid[0][0],grid[0][0])
+dp[0][0] = (grid[0][0],grid[0][0], 0)
 for i in range(1, n):
-    dp[i][0] = (min(dp[i - 1][0][0], grid[i][0]), max(dp[i-1][0][1],  grid[i][0]))
-    dp[0][i] = (min(dp[0][i - 1][0], grid[0][i]), max(dp[0][i-1][1],  grid[0][i]))
+    dp[i][0] = (min(dp[i - 1][0][0], grid[i][0]), max(dp[i-1][0][1],  grid[i][0]), \
+    max(dp[i-1][0][1],  grid[i][0]) - min(dp[i - 1][0][0], grid[i][0]))
+    dp[0][i] = (min(dp[0][i - 1][0], grid[0][i]), max(dp[0][i-1][1],  grid[0][i]), \
+    max(dp[0][i-1][1],  grid[0][i]) - min(dp[0][i - 1][0], grid[0][i]))
 
 for i in range(1, n):
     for j in range(1, n):
@@ -16,42 +18,12 @@ for i in range(1, n):
         dot2 = (min(dp[i][j-1][0],grid[i][j]), max(dp[i][j-1][1],grid[i][j]))
         diff2 = dot2[1]- dot2[0] 
         if diff1 > diff2:
-            dp[i][j] = dot2
+            dp[i][j] = (dot2[0], dot2[1], diff2)
         elif diff1 < diff2:
-            dp[i][j] = dot1
+            dp[i][j] = (dot1[0],dot1[1],diff1)
         else:
-            dp[i][j] = (min(dot1[0],dot2[0]),max(dot1[1],dot2[0]))
-print(dp[n-1][n-1][1] - dp[n-1][n-1][0])
-'''
-dp = [[(101, 0, 101) for _ in range(n)] for _ in range(n)] # (min, max, max-min)
-dxs, dys = [0, 1], [1, 0]
+            dp[i][j] = (min(dot1[0],dot2[0]),min(dot1[1],dot2[1]), diff1)
 
-answer = 0
-for i in range(n):
-    for j in range(n):
-        if i == 0 and j == 0:
-            dp[i][j] = (grid[i][j], grid[i][j], grid[i][j] - grid[i][j])
-        elif i == 0:
-            _min, _max = min(dp[i][j-1][0], grid[i][j]), max(dp[i][j-1][1], grid[i][j]) 
-            _diff = _max - _min
-            dp[i][j] = (_min, _max, _diff)
-        elif j == 0:
-            _min, _max = min(dp[i-1][j][0], grid[i][j]), max(dp[i-1][j][1], grid[i][j]) 
-            _diff = _max - _min
-            dp[i][j] = (_min, _max, _diff)
-        else:
-            dot1 = (min(dp[i-1][j][0], grid[i][j]), max(dp[i-1][j][1], grid[i][j]))
-            dot1_diff = dot1[1]-dot1[0]
-            dot2 = (min(dp[i][j-1][0], grid[i][j]), max(dp[i][j-1][1], grid[i][j]))
-            dot2_diff = dot2[1]-dot2[0]
-
-            if dot1_diff < dot2_diff:
-                dp[i][j] = (dot1[0], dot1[1], dot1_diff)
-            elif dot1_diff > dot2_diff:
-                dp[i][j] = (dot2[0], dot2[1], dot2_diff)
-            else:
-                dp[i][j] = (min(dot1[0],dot2[0]), max(dot1[1], dot2[1]), dot2_diff)
-
-
-
-print(dp[n-1][n-1][2])   '''   
+# for i in range(n):
+#     print(*dp[i])
+print(dp[n-1][n-1][2])
