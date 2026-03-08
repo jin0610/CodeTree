6e68802d0101936a0caa55d5f71471e01017c011
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-09 00:32:34 +0900

    Title: 부분 수열의 합이 M, Time: 65ms, Memory: 17MB, Status: Passed - Codetree

--- 260309/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
+++ 260309/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -1,19 +1,15 @@
 n, m = map(int, input().split())
-A = [0] + list(map(int, input().split()))
+A = list(map(int, input().split()))
 
 # Please write your code here.
-
-A = sorted(A)
 _max = 10001
 dp = [_max for _ in range(m + 1)]
+dp[0] = 0
 
-dp[0] =0
-for i in range(1, n + 1):   # 원소들
-    for j in range(m, -1, -1): # 목표 수
-        if j >= A[i]:
-            if dp[j - A[i]] == _max:
-                continue
-            dp[j] = min(dp[j], dp[j-A[i]] + 1)
+for i in range(n):
+    for j in range(m, -1, -1):
+        if j - A[i] >= 0:
+            dp[j] = min(dp[j], dp[j - A[i]] + 1)
 
 if dp[-1] == _max:
     print(-1)
