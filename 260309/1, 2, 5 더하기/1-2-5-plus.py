96fe6c01dd0ae0197bc857e452ed2df0a67b09d1
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-09 06:59:10 +0900

    Title: 1, 2, 5 더하기, Time: 63ms, Memory: 16MB, Status: Passed - Codetree

--- 260309/1, 2, 5 더하기/1-2-5-plus.py
+++ 260309/1, 2, 5 더하기/1-2-5-plus.py
@@ -2,35 +2,13 @@ n = int(input())
 
 # Please write your code here.
 nums = [1, 2, 5]
-selected = []
-answer =0
-def backtracking(curr_n,cnt):
-    global nums, selected, answer 
-
-    if curr_n == cnt:
-        if sum(selected) == n:
-            answer +=1
-        return
-
-    for num in nums:
-        selected.append(num)
-        backtracking(curr_n + 1, cnt)
-        selected.pop()
-
-for i in range(min(nums), n//min(nums) +1):
-    backtracking(0,i)
-
-print(answer)
-
-'''
 
 dp = [0 for _ in range(n + 1)]
+dp[0] = 1
 dp[1] = 1
-for num in nums:
-    for j in range(1, n + 1):
-        if dp[j - num] != 0:
-            dp[j] += 1
+for i in range(2, n + 1):
+    for num in nums:
+        if i >= num:
+            dp[i] += dp[i-num]
 
-print(dp)
 print(dp[-1] % 10007)
-'''
