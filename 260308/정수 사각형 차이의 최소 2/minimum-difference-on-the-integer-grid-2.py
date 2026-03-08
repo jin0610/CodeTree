a8f66488e1abaa32b05a1348d61229283dbbf749
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-08 15:13:27 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260308/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 260308/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -2,6 +2,27 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
+dp = [[(101, 0) for _ in range(n)] for _ in range(n)]
+
+dp[0][0] = (grid[0][0],grid[0][0])
+for i in range(1, n):
+    dp[i][0] = (min(dp[i - 1][0][0], grid[i][0]), max(dp[i-1][0][1],  grid[i][0]))
+    dp[0][i] = (min(dp[0][i - 1][0], grid[0][i]), max(dp[0][i-1][1],  grid[0][i]))
+
+for i in range(1, n):
+    for j in range(1, n):
+        dot1 = (min(dp[i-1][j][0],grid[i][j]), max(dp[i-1][j][1],grid[i][j]))
+        diff1 =  dot1[1]- dot1[0]
+        dot2 = (min(dp[i][j-1][0],grid[i][j]), max(dp[i][j-1][1],grid[i][j]))
+        diff2 = dot2[1]- dot2[0] 
+        if diff1 > diff2:
+            dp[i][j] = dot2
+        elif diff1 < diff2:
+            dp[i][j] = dot1
+        else:
+            dp[i][j] = (min(dot1[0],dot2[0]),max(dot1[1],dot2[0]))
+print(dp[n-1][n-1][1] - dp[n-1][n-1][0])
+'''
 dp = [[(101, 0, 101) for _ in range(n)] for _ in range(n)] # (min, max, max-min)
 dxs, dys = [0, 1], [1, 0]
 
@@ -26,8 +47,11 @@ for i in range(n):
 
             if dot1_diff < dot2_diff:
                 dp[i][j] = (dot1[0], dot1[1], dot1_diff)
-            else:
+            elif dot1_diff > dot2_diff:
                 dp[i][j] = (dot2[0], dot2[1], dot2_diff)
+            else:
+                dp[i][j] = (min(dot1[0],dot2[0]), max(dot1[1], dot2[1]), dot2_diff)
 
 
-print(dp[n-1][n-1][2])      
+
+print(dp[n-1][n-1][2])   '''   
