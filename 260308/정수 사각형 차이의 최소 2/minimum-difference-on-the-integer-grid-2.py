03f7e6007832d27ec35549d6f842c7acda3de0bf
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-08 15:56:46 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 245ms, Memory: 23MB, Status: Passed - Codetree

--- 260308/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 260308/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,29 +1,32 @@
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-dp = [[(101, 0, 101) for _ in range(n)] for _ in range(n)]
+def check():
+    d = [[1e9]*n for _ in range(n)]
+    d[0][0] = graph[0][0]
 
-dp[0][0] = (grid[0][0],grid[0][0], 0)
-for i in range(1, n):
-    dp[i][0] = (min(dp[i - 1][0][0], grid[i][0]), max(dp[i-1][0][1],  grid[i][0]), \
-    max(dp[i-1][0][1],  grid[i][0]) - min(dp[i - 1][0][0], grid[i][0]))
-    dp[0][i] = (min(dp[0][i - 1][0], grid[0][i]), max(dp[0][i-1][1],  grid[0][i]), \
-    max(dp[0][i-1][1],  grid[0][i]) - min(dp[0][i - 1][0], grid[0][i]))
-
-for i in range(1, n):
     for j in range(1, n):
-        dot1 = (min(dp[i-1][j][0],grid[i][j]), max(dp[i-1][j][1],grid[i][j]))
-        diff1 =  dot1[1]- dot1[0]
-        dot2 = (min(dp[i][j-1][0],grid[i][j]), max(dp[i][j-1][1],grid[i][j]))
-        diff2 = dot2[1]- dot2[0] 
-        if diff1 > diff2:
-            dp[i][j] = (dot2[0], dot2[1], diff2)
-        elif diff1 < diff2:
-            dp[i][j] = (dot1[0],dot1[1],diff1)
-        else:
-            dp[i][j] = (min(dot1[0],dot2[0]),max(min(dp[i-1][j][1],dp[i][j-1][1]),grid[i][j]), diff1)
+        d[0][j] = max(d[0][j-1], graph[0][j])
+    
+    for i in range(1, n):
+        d[i][0] = max(d[i-1][0], graph[i][0])
 
-# for i in range(n):
-#     print(*dp[i])
-print(dp[n-1][n-1][2])
+    for i in range(1, n):
+        for j in range(1, n):
+            d[i][j] = max(min(d[i-1][j], d[i][j-1]), graph[i][j])
+    
+    return d[n-1][n-1]  # 1e9면 경로 없음!
+
+
+res = 1e9  # |최대-최소| 최소!
+for low in range(1, 101):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] < low:
+                graph[i][j] = 1e9
+
+    ans = check()
+    if ans < 1e9:
+        res = min(res, abs(ans-low))
+
+print(res)
