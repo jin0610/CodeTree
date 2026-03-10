f309ddf5187a04a50a2f176c1d52669376541e02
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-10 14:43:32 +0900

    Title: 가장 많이 겹치는 구간, Time: 1076ms, Memory: 31MB, Status: Time Limit Exceed - Codetree

--- 260310/가장 많이 겹치는 구간/section-with-maximum-overlap.py
+++ 260310/가장 많이 겹치는 구간/section-with-maximum-overlap.py
@@ -4,7 +4,7 @@ intervals = [tuple(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 # _max_point = sorted(intervals, key = lambda x : -x[1])[0]
 
-check = [0] * (20001)
+check = [0] * (200001)
 _max = 0
 for x1, x2 in intervals:
     _max = max(_max, x2)
