cfc3f929c8a6bba43fc920211d710ff1662ef66f
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-03-11 15:28:56 +0900

    Title: 가까운 숫자, Time: 560ms, Memory: 34MB, Status: Passed - Codetree

--- 260311/가까운 숫자/nearest-number.py
+++ 260311/가까운 숫자/nearest-number.py
@@ -3,17 +3,23 @@ import sys
 
 n = int(input())
 arr = list(map(int, input().split()))
-
-s = SortedSet()
-s.add(0)
+s = SortedSet([0])
 
 length = sys.maxsize
 for elem in arr:
     s.add(elem)
+
+    # 오른쪽 간격
     x2 = s.bisect_right(elem)
-    if x2 >= len(s):
-        x2 = len(s) - 1
-        
-    x1 = x2 - 1
-    length = min(length, s[x2] - s[x1])
+    if x2 < len(s):
+        x1 = x2 - 1
+        length = min(length, s[x2] - s[x1])
+
+    # 왼쪽 간격
+    x2 = s.bisect_left(elem)
+    if x2 > 0:
+        x1 = x2 - 1
+        length = min(length, s[x2] - s[x1])
+
     print(length)
+    
