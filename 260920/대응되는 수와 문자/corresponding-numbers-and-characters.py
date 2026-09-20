0240c4718dca472e04415a72660eeeb3c23a42e9
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-09-20 12:04:53 +0900

    [Passed] 대응되는 수와 문자 (402ms, 37MB)

--- 260920/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 260920/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -1,17 +1,16 @@
 N, M = map(int, input().split())
 
+word = [""] + [input() for _ in range(N)]
+
 word_dict = dict()
-reverse_dict = dict()
-for n in range(N):
-    w = input()
-    word_dict[w] = n + 1
-    reverse_dict[n+1] = w
+for i, w in enumerate(word):
+    word_dict[w] = i
 
 for m in range(M):
     w = input()
-    try:
-        w = int(w)
-        print(reverse_dict[w])
-    except:
+    if w.isdigit():
+        print(word[int(w)])
+
+    else:
         print(word_dict[w])
     
