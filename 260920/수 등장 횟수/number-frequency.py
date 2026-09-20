e4598a8f817fbb4a899938234173162fd90f4420
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-09-20 11:12:22 +0900

    [Passed] 수 등장 횟수 (115ms, 28MB)

--- 260920/수 등장 횟수/number-frequency.py
+++ 260920/수 등장 횟수/number-frequency.py
@@ -1,5 +1,6 @@
 N, M = map(int, input().split())
 arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
 
 num_dict = dict()
 for n in arr1:
@@ -8,9 +9,9 @@ for n in arr1:
     else:
         num_dict[n] = 1
 
-arr2 = list(map(int, input().split()))
 for m in arr2:
     if m in num_dict.keys():
         print(num_dict[m], end=' ')
     else:
         print(0, end=' ')
+
