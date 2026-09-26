536e946389401ec1842e28643d1811fa63c55a92
jin0610 <87145895+jin0610@users.noreply.github.com>
2026-09-27 00:20:25 +0900

    [Passed] 트로미노 (54ms, 16MB)

--- 260927/트로미노/tromino.py
+++ 260927/트로미노/tromino.py
@@ -1,38 +1,64 @@
-N, M = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(N)]
+# 변수 선언 및 입력:
 
-answer = 0
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-def in_range(n, m):
-    return n >= 0 and n < N and m >= 0 and m < M
+# 가능한 모든 모양을 전부 적어줍니다.
+shapes = [
+    [[1, 1, 0],
+    [1, 0, 0],
+    [0, 0, 0]],
 
-for n in range(N):
-    for m in range(M):
-    
-        # 3 x 1
-        if in_range(n - 1, m) and in_range(n + 1, m):
-            answer = max(answer, grid[n - 1][m] + grid[n][m] + grid[n + 1][m])
+    [[1, 1, 0],
+    [0, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0]],
+]
+
+# 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환합니다.
+def get_max_sum(x, y):
+    max_sum = 0
+    for i in range(6):
+        is_possible = True
+        sum_of_nums = 0
+        for dx in range(0, 3):
+            for dy in range(0, 3):
+                if shapes[i][dx][dy] == 0:
+                    continue
+                if x + dx >= n or y + dy >= m:
+                    is_possible = False
+                else:
+                    sum_of_nums += grid[x + dx][y + dy]
         
-        # 1 x 3
-        if in_range(n, m - 1) and in_range(n, m + 1):
-            answer = max(answer, grid[n][m - 1] + grid[n][m] + grid[n][m + 1])
+        if is_possible:
+            max_sum = max(max_sum, sum_of_nums)
 
-        # ㄱ
-        if in_range(n, m - 1) and in_range(n + 1, m):
-            answer = max(answer, grid[n][m - 1] + grid[n][m] + grid[n + 1][m])
+    return max_sum
 
-        # ㄴ
-        if in_range(n - 1, m) and in_range(n, m + 1):
-            answer = max(answer, grid[n - 1][m] + grid[n][m] + grid[n][m + 1])
-        
-        # r
-        if in_range(n - 1, m + 1) and in_range(n - 1, m):
-            answer = max(answer, grid[n - 1][m] + grid[n][m] + grid[n - 1][m + 1])
 
-        # ㄴ 반대
-        if in_range(n, m - 1) and in_range(n - 1, m):
-            answer = max(answer, grid[n][m - 1] + grid[n][m] + grid[n - 1][m])
+ans = 0
 
-print(answer)
-        
-        
+# 격자의 각 위치에 대하여 탐색하여줍니다.
+for i in range(n):
+    for j in range(m):
+        ans = max(ans, get_max_sum(i, j))
+
+print(ans)
